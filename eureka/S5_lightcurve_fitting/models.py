b9b8ac2913555860edd7107c08372e10b074029e
Laura Kreidberg <kreidberg@mpia.de>
2020-08-07 17:00:34 +0200

    add Observations class

--- eureka/S5_lightcurve_fitting/models.py
+++ eureka/S5_lightcurve_fitting/models.py
@@ -17,6 +17,7 @@ except ImportError:
 from bokeh.plotting import figure, show
 
 from .parameters import Parameters
+from .observations import Observations
 from .utils import COLORS
 from .limb_darkening_fit import ld_profile
 
