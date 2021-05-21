4dba285fa7938da6680feb2bce96d17ce0e005f5
Kevin B. Stevenson <kbstevenson@gmail.com>
2021-05-21 17:52:43 -0400

    Wrote basic NIRCam reduction

--- eureka/S3_data_reduction/nircam.py
+++ eureka/S3_data_reduction/nircam.py
@@ -1,2 +1,76 @@
 
 # NIRCam specific rountines go here
+import numpy as np
+from astropy.io import fits
+import sigrej, optspex
+
+# Read FITS file from JWST's NIRCam instrument
+def read(filename, returnHdr=True):
+    '''
+    Reads single FITS file from JWST's NIRCam instrument.
+
+    Parameters
+    ----------
+    filename          : Single filename to read
+    returnHdr         : Set True to return header files
+
+    Returns
+    -------
+    data            : Array of data frames
+    err             : Array of uncertainty frames
+    hdr             : List of header files
+    master_hdr      : List of master header files
+
+    History
+    -------
+    Written by Kevin Stevenson          November 2012
+    Updated for NIRCam (KBS)            May 2021
+
+    '''
+    assert isinstance(filename, str)
+
+    hdulist = fits.open(filename)
+
+    # Load master and science headers
+    mhdr    = hdulist[0].header
+    shdr    = hdulist['SCI',1].header
+
+    intstart    = mhdr['INTSTART']
+    intend      = mhdr['INTEND']
+
+    data    = hdulist['SCI',1].data
+    err     = hdulist['ERR',1].data
+    dq      = hdulist['DQ',1].data
+    wave    = hdulist['WAVELENGTH',1].data
+    v0      = hdulist['VAR_RNOISE',1].data
+    int_times = hdulist['INT_TIMES',1].data[intstart-1:intend]
+
+    if returnHdr:
+        return data, err, dq, wave, v0, int_times, mhdr, shdr
+    else:
+        return data, err, dq, wave, v0, int_times
+
+def flag_bg(data, err, mask, y1, y2, bg_thresh):
+    '''
+    Outlier rejection of sky background along time axis
+    '''
+    bgdata1 = data[:,  :y1]
+    bgmask1 = mask[:,  :y1]
+    bgdata2 = data[:,y2:  ]
+    bgmask2 = mask[:,y2:  ]
+    bgerr1  = np.median(err[:,  :y1])
+    bgerr2  = np.median(err[:,y2:  ])
+    estsig1 = [bgerr1 for j in range(len(bg_thresh))]
+    estsig2 = [bgerr2 for j in range(len(bg_thresh))]
+    mask[:,  :y1] = sigrej.sigrej(bgdata1, bg_thresh, bgmask1, estsig1)
+    mask[:,y2:  ] = sigrej.sigrej(bgdata2, bg_thresh, bgmask2, estsig2)
+
+    return mask
+
+def fit_bg(data, mask, y1, y2, bg_deg, p3thresh, n, isplots=False):
+    '''
+
+    '''
+    bg, mask = optspex.fitbg(data, mask, y1, y2, deg=bg_deg,
+                             threshold=p3thresh, isrotate=2, isplots=isplots)
+    return (bg, mask, n)
--- eureka/S3_data_reduction/s3_reduce.py
+++ eureka/S3_data_reduction/s3_reduce.py
@@ -32,6 +32,8 @@ import numpy as np
 import logedit
 import readECF as rd
 import manageevent as me
+import sort_nicely as sn
+import matplotlib.pyplot as plt
 
 class Event():
   def __init__(self):
@@ -84,11 +86,148 @@ def reduceJWST(eventlabel, isplots=False):
     ecf     = rd.read_ecf(ecffile)
     rd.store_ecf(ev, ecf)
 
+    # Load instrument module
+    exec('import ' + ev.inst + ' as inst', globals())
+    reload(inst)
+
     # Open new log file
     ev.logname  = './'+ev.dirname + '/S3_' + ev.eventlabel + ".log"
     log         = logedit.Logedit(ev.logname)
     log.writelog("\nStarting Stage 3 Reduction")
 
+    # Create list of file segments
+    ev.segment_list = []
+    for fname in os.listdir(ev.topdir + ev.datadir):
+        if fname.endswith(ev.suffix + '.fits'):
+            ev.segment_list.append(ev.topdir + ev.datadir +'/'+ fname)
+    ev.segment_list = sn.sort_nicely(ev.segment_list)
+    num_data_files = len(ev.segment_list)
+    log.writelog(f'\nFound {num_data_files} data file(s) ending in {ev.suffix}.fits')
+
+    ev.stdspec = np.array([])
+    ev.stdvar  = np.array([])
+    # Loop over each segment
+    for m in range(num_data_files):
+        # Report progress
+
+        # Read in data frame and header
+        log.writelog(f'Reading file {m}')
+        data, err, dq, wave, v0, int_times, mhdr, shdr = inst.read(ev.segment_list[m], returnHdr=True)
+        # Get number of integrations and frame dimensions
+        n_int, ny, nx = data.shape
+        intstart = mhdr['INTSTART']
+        # Locate science image
+        xref_sci = shdr['XREF_SCI']
+        yref_sci = shdr['YREF_SCI']
+        # Record integration mid-times in BJD_TDB
+        bjdtdb = int_times['int_mid_BJD_TDB']
+        # Trim data to subarray region of interest
+        subdata  = data[:,ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
+        suberr   = err [:,ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
+        subdq    = dq  [:,ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
+        subwave  = wave[  ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
+        subv0    = v0  [:,ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
+        subny    = ev.ywindow[1]-ev.ywindow[0]
+        subnx    = ev.xwindow[1]-ev.xwindow[0]
+        # Create bad pixel mask (1 = good, 0 = bad)
+        # FINDME: Will want to use DQ array in the future to flag certain pixels
+        submask = np.ones(subdata.shape)
+        # FINDME: Need to convert flux from MJy/sr to electrons
+        # FINDME: err and v0 are all NaNs
+        if np.sum(np.isnan(suberr)) > 30000:
+            log.writelog("ERR array is all NaNs... Assuming ERR = sqrt(DATA), which is very wrong if still in MJy/sr!!!")
+            suberr = np.sqrt(np.abs(subdata))
+        # Perform outlier rejection of sky background along time axis
+        log.writelog('Performing background outlier rejection')
+        bg_y1    = int(yref_sci - ev.bg_hw)
+        bg_y2    = int(yref_sci + ev.bg_hw)
+        submask = inst.flag_bg(subdata, suberr, submask, bg_y1, bg_y2, ev.bg_thresh)
+        log.writelog('Performing background subtraction')
+        # Write background
+        def writeBG(arg):
+            bg_data, bg_mask, n = arg
+            subbg[n] = bg_data
+            submask[n] = bg_mask
+            return
+        # Compute background for each integration
+        subbg  = np.zeros((subdata.shape))
+        if ev.ncpu == 1:
+            # Only 1 CPU
+            for n in range(n_int):
+                # Fit sky background with out-of-spectra data
+                writeBG(inst.fit_bg(subdata[n], submask[n], bg_y1, bg_y2, ev.bg_deg, ev.p3thresh, n, isplots))
+        else:
+            # Multiple CPUs
+            pool = mp.Pool(ev.ncpu)
+            for n in range(n_int):
+                res = pool.apply_async(inst.fit_bg, args=(subdata[n], submask[n], bg_y1, bg_y2, bg_deg, ev.p3thresh, n, isplots), callback=writeBG)
+            pool.close()
+            pool.join()
+            res.wait()
+        # Calculate variance
+        # bgerr       = np.std(bg, axis=1)/np.sqrt(np.sum(mask, axis=1))
+        # bgerr[np.where(np.isnan(bgerr))] = 0.
+        # v0[np.where(np.isnan(v0))] = 0.   # FINDME: v0 is all NaNs
+        # v0         += np.mean(bgerr**2)
+        # variance    = abs(data) / gain + ev.v0    # FINDME: Gain reference file: 'crds://jwst_nircam_gain_0056.fits'
+        #variance    = abs(subdata*submask) / gain + v0
+
+        # Perform background subtraction
+        subdata    -= subbg
+        # Calulate drift2D
+        # print("Calculating 2D drift...")
+        # print("Performing rough, pixel-scale drift correction...")
+        # Outlier rejection of full frame along time axis
+        # print("Performing full-frame outlier rejection...")
+
+        if isplots >= 3:
+            for n in range(n_int):
+                plt.figure(3001)
+                plt.clf()
+                plt.suptitle(str(intstart+n))
+                plt.subplot(211)
+                plt.imshow(subdata[n]*submask[n], origin='lower', aspect='auto', vmin=0, vmax=10000)
+                #plt.imshow(subdata[n], origin='lower', aspect='auto', vmin=0, vmax=10000)
+                plt.subplot(212)
+                #plt.imshow(submask[i], origin='lower', aspect='auto', vmax=1)
+                mean = np.median(subbg[n])
+                std  = np.std(subbg[n])
+                plt.imshow(subbg[n], origin='lower', aspect='auto',vmin=mean-3*std,vmax=mean+3*std)
+                #plt.imshow(submask[n], origin='lower', aspect='auto', vmin=0, vmax=1)
+                plt.savefig(ev.dirname+'/figs/fig3001-'+str(intstart+n)+'-Image+Background.png')
+                #plt.pause(0.1)
+        # print("Performing sub-pixel drift correction...")
+        # Extract standard spectrum and its variance
+        stdspec     = np.sum(subdata, axis=1)
+        stdvar      = np.sum(suberr**2, axis=1)
+        # Compute fraction of masked pixels within regular spectral extraction window
+        #numpixels   = 2.*ev.spec_width*subnx
+        #fracMaskReg = (numpixels - np.sum(apmask,axis=(2,3)))/numpixels
+
+        # Compute median frame
+        ev.meddata  = np.median(subdata, axis=0)
+
+        # Extract optimal spectrum with uncertainties
+        # print("Performing optimal spectral extraction...")
+
+        # Plotting results
+        if isplots >= 3:
+            for n in range(n_int):
+                plt.figure(3002)
+                plt.clf()
+                plt.suptitle(str(intstart+n))
+                plt.errorbar(range(subnx), stdspec[n], yerr=np.sqrt(stdvar[n]), fmt='-', color='C1', ecolor='C0', label='Std Spec')
+                #plt.errorbar(range(subnx), spectra[n], specerr[n], fmt='-', color='C2', ecolor='C4', label='Optimal Spec')
+                plt.legend(loc='best')
+                plt.savefig(ev.dirname+'/figs/fig3002-'+str(intstart+n)+'-Spectrum.png')
+                #plt.pause(0.1)
+        # Append results
+        if len(ev.stdspec) == 0:
+            ev.stdspec = stdspec
+            ev.stdvar  = stdvar
+        else:
+            ev.stdspec = np.append(ev.stdspec, stdspec, axis=0)
+            ev.stdvar  = np.append(ev.stdvar, stdvar, axis=0)
 
     # Calculate total time
     total = (time.time() - t0)/60.
