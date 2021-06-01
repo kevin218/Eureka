48dcd09303e18bdc4f85797e9739339d9e35f980
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2021-06-01 05:52:02 +0200

    solved more merge conflicts

--- demos/S3/run_eureka.py
+++ demos/S3/run_eureka.py
@@ -3,7 +3,6 @@ import sys, os, time
 #sys.path.append('../..')
 #sys.path.append('/Users/stevekb1/Documents/code/Eureka/Eureka')
 sys.path.append('/home/zieba/Desktop/Projects/Open_source/Eureka')
-
 from importlib import reload
 import eureka.S3_data_reduction.s3_reduce as s3
 reload(s3)
