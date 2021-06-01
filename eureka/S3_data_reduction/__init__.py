39e210983b07579648e312aa254ff0e072c1d441
Kevin B. Stevenson <kbstevenson@gmail.com>
2021-06-01 16:27:41 -0400

    Write basic Stage 4, convert DN to e-

--- eureka/S3_data_reduction/__init__.py
+++ eureka/S3_data_reduction/__init__.py
@@ -3,9 +3,7 @@ from . import miri
 from . import niriss
 from . import optspex
 from . import sigrej
-from . import wfc3_scan
 from . import bright2flux
 from . import nircam
 from . import nirspec
 from . import s3_reduce
-from . import wfc3_1reduce
