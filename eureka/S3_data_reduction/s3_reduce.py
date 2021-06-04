6a11ceb56a6fd264bcc168a64034ce3db0795608
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2021-06-04 13:05:11 +0200

    replaced s3_reduce_short.py with s3_reduce.py

--- eureka/S3_data_reduction/s3_reduce.py
+++ eureka/S3_data_reduction/s3_reduce.py
@@ -5,28 +5,27 @@
 """
 # Proposed Steps
 # -------- -----
-# 1.  Read in all data frames and header info from Stage 2 data products
-# 2.  Record JD and other relevant header information
-# 3.  Apply light-time correction (if necessary)
+# 1.  Read in all data frames and header info from Stage 2 data products DONE
+# 2.  Record JD and other relevant header information DONE
+# 3.  Apply light-time correction (if necessary) DONE
 # 4.  Calculate trace and 1D+2D wavelength solutions (if necessary)
 # 5.  Make flats, apply flat field correction (Stage 2)
-# 6.  Manually mask regions
+# 6.  Manually mask regions DONE
 # 7.  Compute difference frames OR slopes (Stage 1)
-# 8.  Perform outlier rejection of BG region
-# 9.  Background subtraction
+# 8.  Perform outlier rejection of BG region DONE
+# 9.  Background subtraction DONE
 # 10. Compute 2D drift, apply rough (integer-pixel) correction
 # 11. Full-frame outlier rejection for time-series stack of NDRs
 # 12. Apply sub-pixel 2D drift correction
-# 13. Extract spectrum through summation
-# 14. Compute median frame
-# 15. Optimal spectral extraction
+# 13. Extract spectrum through summation DONE
+# 14. Compute median frame DONE
+# 15. Optimal spectral extraction DONE
 # 16. Save Stage 3 data products
-# 17. Produce plots
+# 17. Produce plots DONE
 """
 
 import sys, os, time
 import numpy as np
-import matplotlib.pyplot as plt
 import multiprocessing as mp
 from ..lib import logedit
 from ..lib import readECF as rd
@@ -36,7 +35,8 @@ from . import bright2flux as b2f
 from . import optspex
 from importlib import reload
 from ..lib import savetable
-from ..lib import barycorr
+from ..lib import util
+from . import plots_s3
 reload(optspex)
 reload(b2f)
 
@@ -127,21 +127,10 @@ def reduceJWST(eventlabel, isplots=False, testing=False):
         # Locate source postion
         src_xpos = shdr['SRCXPOS']-ev.xwindow[0]
         src_ypos = shdr['SRCYPOS']-ev.ywindow[0]
-        # Target Coordinates
-        ev.ra  = mhdr['TARG_RA'] * np.pi / 180.0  # stores right ascension
-        ev.dec = mhdr['TARG_DEC'] * np.pi / 180.0  # stores right ascension
         # Record integration mid-times in BJD_TDB
-        mjdutc = int_times['int_mid_MJD_UTC']
-        #bjdtdb = int_times['int_mid_BJD_TDB']
-        bjdtdb = barycorr.to_bjdtdb(ev, mjdutc, m)
+        bjdtdb = int_times['int_mid_BJD_TDB']
         # Trim data to subarray region of interest
-        subdata     = data    [:,ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
-        suberr      = err     [:,ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
-        subdq       = dq      [:,ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
-        subwave     = wave    [  ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
-        subv0       = v0      [:,ev.ywindow[0]:ev.ywindow[1],ev.xwindow[0]:ev.xwindow[1]]
-        subny       = ev.ywindow[1]-ev.ywindow[0]
-        subnx       = ev.xwindow[1]-ev.xwindow[0]
+        subdata, suberr, subdq, subwave, subv0, subny, subnx = util.trim(ev, data,err, dq, wave, v0)
         # Create bad pixel mask (1 = good, 0 = bad)
         # FINDME: Will want to use DQ array in the future to flag certain pixels
         submask = np.ones(subdata.shape)
@@ -150,28 +139,16 @@ def reduceJWST(eventlabel, isplots=False, testing=False):
             #log.writelog('Converting from brightness to flux units')
             #subdata, suberr, subv0 = b2f.bright2flux(subdata, suberr, subv0, shdr['PIXAR_A2'])
             # Convert from brightness units (MJy/sr) to DNs
-            log.writelog('  Converting from brightness units (MJy/sr) to electronss')
+            log.writelog('  Converting from brightness units (MJy/sr) to electrons')
             photfile = ev.topdir + ev.ancildir +'/'+ mhdr['R_PHOTOM'][7:]
             subdata, suberr, subv0 = b2f.bright2dn(subdata, suberr, subv0, subwave, photfile, mhdr, shdr)
             gainfile = ev.topdir + ev.ancildir +'/'+ mhdr['R_GAIN'][7:]
             subdata, suberr, subv0 = b2f.dn2electrons(subdata, suberr, subv0, gainfile, mhdr, ev.ywindow, ev.xwindow)
 
         # Check if arrays have NaNs
-        if np.sum(np.isnan(subdata)) > 0:
-            log.writelog("  WARNING: DATA array has NaNs.  Your subregion is probably off the edge of the detector subarray. Masking NaN region and continuing, but you should probably stop and reconsider your choices.")
-            inan = np.where(np.isnan(subdata))
-            #subdata[inan]  = 0
-            submask[inan]  = 0
-        if np.sum(np.isnan(suberr)) > 0:
-            log.writelog("  WARNING: ERR array has NaNs. Your subregion is probably off the edge of the detector subarray. Masking NaN region and continuing, but you should probably stop and reconsider your choices.")
-            inan = np.where(np.isnan(suberr))
-            #suberr[inan]  = np.sqrt(np.abs(subdata[inan]))
-            submask[inan]  = 0
-        if np.sum(np.isnan(subv0)) > 0:
-            log.writelog("  WARNING: v0 array has NaNs. Your subregion is probably off the edge of the detector subarray. Masking NaN region and continuing, but you should probably stop and reconsider your choices.")
-            inan = np.where(np.isnan(subv0))
-            #subv0[inan]   = 0
-            submask[inan]  = 0
+        submask = util.check_nans(subdata, submask, log)
+        submask = util.check_nans(suberr, submask, log)
+        submask = util.check_nans(subv0, submask, log)
 
         # Manually mask regions [colstart, colend, rowstart, rowend]
         if hasattr(ev, 'manmask'):
@@ -186,38 +163,9 @@ def reduceJWST(eventlabel, isplots=False, testing=False):
         bg_y2    = int(src_ypos + ev.bg_hw)
         submask = inst.flag_bg(subdata, suberr, submask, bg_y1, bg_y2, ev.bg_thresh)
 
-        # Write background
-        def writeBG(arg):
-            bg_data, bg_mask, n = arg
-            subbg[n] = bg_data
-            submask[n] = bg_mask
-            return
-        # Compute background for each integration
-        log.writelog('  Performing background subtraction')
-        subbg  = np.zeros((subdata.shape))
-        if ev.ncpu == 1:
-            # Only 1 CPU
-            for n in range(n_int):
-                # Fit sky background with out-of-spectra data
-                writeBG(inst.fit_bg(subdata[n], submask[n], bg_y1, bg_y2, ev.bg_deg, ev.p3thresh, n, isplots))
-        else:
-            # Multiple CPUs
-            pool = mp.Pool(ev.ncpu)
-            for n in range(n_int):
-                res = pool.apply_async(inst.fit_bg, args=(subdata[n], submask[n], bg_y1, bg_y2, ev.bg_deg, ev.p3thresh, n, isplots), callback=writeBG)
-            pool.close()
-            pool.join()
-            res.wait()
-        # Calculate variance
-        # bgerr       = np.std(bg, axis=1)/np.sqrt(np.sum(mask, axis=1))
-        # bgerr[np.where(np.isnan(bgerr))] = 0.
-        # v0[np.where(np.isnan(v0))] = 0.   # FINDME: v0 is all NaNs
-        # v0         += np.mean(bgerr**2)
-        # variance    = abs(data) / gain + ev.v0    # FINDME: Gain reference file: 'crds://jwst_nircam_gain_0056.fits'
-        #variance    = abs(subdata*submask) / gain + v0
 
-        # Perform background subtraction
-        subdata    -= subbg
+        subbg, submask, subdata = util.BGsubtraction(ev, log, n_int, bg_y1, bg_y2,subdata, submask, isplots)
+
 
         # Calulate drift2D
         # print("Calculating 2D drift...")
@@ -229,21 +177,10 @@ def reduceJWST(eventlabel, isplots=False, testing=False):
 
         if isplots >= 3:
             for n in range(n_int):
-                plt.figure(3301)
-                plt.clf()
-                plt.suptitle(str(intstart+n))
-                plt.subplot(211)
-                max = np.max(subdata[n]*submask[n])
-                plt.imshow(subdata[n]*submask[n], origin='lower', aspect='auto', vmin=0, vmax=max/10)
-                #plt.imshow(subdata[n], origin='lower', aspect='auto', vmin=0, vmax=10000)
-                plt.subplot(212)
-                #plt.imshow(submask[i], origin='lower', aspect='auto', vmax=1)
-                median = np.median(subbg[n])
-                std  = np.std(subbg[n])
-                plt.imshow(subbg[n], origin='lower', aspect='auto',vmin=median-3*std,vmax=median+3*std)
-                #plt.imshow(submask[n], origin='lower', aspect='auto', vmin=0, vmax=1)
-                plt.savefig(ev.workdir+'/figs/fig3301-'+str(intstart+n)+'-Image+Background.png')
-                #plt.pause(0.1)
+                #make image+background plots
+                plots_s3.image_and_background(ev, intstart, n, subdata, submask, subbg)
+
+
         # print("Performing sub-pixel drift correction...")
 
         # Select only aperture region
@@ -276,15 +213,9 @@ def reduceJWST(eventlabel, isplots=False, testing=False):
         # Plotting results
         if isplots >= 3:
             for n in range(n_int):
-                plt.figure(3302)
-                plt.clf()
-                plt.suptitle(str(intstart+n))
-                plt.plot(range(subnx), stdspec[n], '-', color='C1', label='Std Spec')
-                #plt.errorbar(range(subnx), stdspec[n], yerr=np.sqrt(stdvar[n]), fmt='-', color='C1', ecolor='C0', label='Std Spec')
-                plt.errorbar(range(subnx), optspec[n], opterr[n], fmt='-', color='C2', ecolor='C2', label='Optimal Spec')
-                plt.legend(loc='best')
-                plt.savefig(ev.workdir+'/figs/fig3302-'+str(intstart+n)+'-Spectrum.png')
-                #plt.pause(0.1)
+                #make optimal spectrum plot
+                plots_s3.optimal_spectrum(ev, intstart, n, subnx, stdspec, optspec, opterr)
+
         # Append results
         if len(ev.stdspec) == 0:
             ev.wave_2d  = subwave
@@ -293,14 +224,12 @@ def reduceJWST(eventlabel, isplots=False, testing=False):
             ev.stdvar   = stdvar
             ev.optspec  = optspec
             ev.opterr   = opterr
-            ev.mjdutc   = mjdutc
             ev.bjdtdb   = bjdtdb
         else:
             ev.stdspec  = np.append(ev.stdspec, stdspec, axis=0)
             ev.stdvar   = np.append(ev.stdvar, stdvar, axis=0)
             ev.optspec  = np.append(ev.optspec, optspec, axis=0)
             ev.opterr   = np.append(ev.opterr, opterr, axis=0)
-            ev.mjdutc   = np.append(ev.mjdutc, mjdutc, axis=0)
             ev.bjdtdb   = np.append(ev.bjdtdb, bjdtdb, axis=0)
 
     # Calculate total time
@@ -318,31 +247,7 @@ def reduceJWST(eventlabel, isplots=False, testing=False):
     log.writelog('Generating figures')
     if isplots >= 1:
         # 2D light curve without drift correction
-        plt.figure(3101, figsize=(8,8))  #ev.n_files/20.+0.8))
-        plt.clf()
-        wmin        = ev.wave_1d.min()
-        wmax        = ev.wave_1d.max()
-        n_int, nx   = ev.optspec.shape
-        #iwmin       = np.where(ev.wave[src_ypos]>wmin)[0][0]
-        #iwmax       = np.where(ev.wave[src_ypos]>wmax)[0][0]
-        vmin        = 0.97
-        vmax        = 1.03
-        #normspec    = np.mean(ev.optspec,axis=1)/np.mean(ev.optspec[ev.inormspec[0]:ev.inormspec[1]],axis=(0,1))
-        normspec    = ev.optspec/np.mean(ev.optspec,axis=0)
-        plt.imshow(normspec,origin='lower', aspect='auto',extent=[wmin,wmax,0,n_int],vmin=vmin,vmax=vmax,cmap=plt.cm.RdYlBu_r)
-        ediff       = np.zeros(n_int)
-        for m in range(n_int):
-            ediff[m]    = 1e6*np.median(np.abs(np.ediff1d(normspec[m])))
-            # plt.scatter(ev.wave[src_ypos], np.zeros(nx)+m, c=normspec[m],
-            #             s=14,linewidths=0,vmin=vmin,vmax=vmax,marker='s',cmap=plt.cm.RdYlBu_r)
-        plt.title("MAD = "+str(np.round(np.mean(ediff),0).astype(int)) + " ppm")
-        #plt.xlim(wmin,wmax)
-        #plt.ylim(0,n_int)
-        plt.ylabel('Integration Number')
-        plt.xlabel('Wavelength ($\mu m$)')
-        plt.colorbar(label='Normalized Flux')
-        plt.tight_layout()
-        plt.savefig(ev.workdir+'/figs/fig3101-2D_LC.png')
+        plots_s3.lc_nodriftcorr(ev)
 
     log.closelog()
     return ev
