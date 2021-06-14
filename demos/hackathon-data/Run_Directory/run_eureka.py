319ef59a27a5df3d49fd23fca8b4f8231cf5002c
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2021-06-14 12:36:06 +0200

    added hackathon data

---
+++ demos/hackathon-data/Run_Directory/run_eureka.py
@@ -0,0 +1,17 @@
+
+import sys, os, time
+#sys.path.append('../..')
+#sys.path.append('/Users/stevekb1/Documents/code/Eureka/Eureka')
+#sys.path.append('/Users/kreidberg/Desktop/Projects/OpenSource/Eureka/')
+sys.path.append('/home/zieba/Desktop/Projects/Open_source/Eureka')
+from importlib import reload
+import eureka.S3_data_reduction.s3_reduce as s3
+import eureka.S4_generate_lightcurves.s4_genLC as s4
+
+eventlabel = 'wasp43b'
+
+reload(s3)
+ev3 = s3.reduceJWST(eventlabel, isplots=0, testing=False)
+
+reload(s4)
+ev4 = s4.lcJWST(ev3.eventlabel, ev3.workdir, ev=ev3, isplots=0)
