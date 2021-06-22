db0a4169c6bbaca1e927a20c8c7a0f2d2860f2d1
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2021-06-22 16:21:33 +0200

    commented out sys.path.add in run_script.py

--- demos/S3/run_eureka.py
+++ demos/S3/run_eureka.py
@@ -2,7 +2,7 @@ import sys, os, time
 #sys.path.append('../..')
 #sys.path.append('/Users/stevekb1/Documents/code/Eureka/Eureka')
 #sys.path.append('/Users/kreidberg/Desktop/Projects/OpenSource/Eureka/')
-sys.path.append('/home/zieba/Desktop/Projects/Open_source/Eureka')
+#sys.path.append('/home/zieba/Desktop/Projects/Open_source/Eureka')
 from importlib import reload
 import eureka.S3_data_reduction.s3_reduce as s3
 import eureka.S4_generate_lightcurves.s4_genLC as s4
