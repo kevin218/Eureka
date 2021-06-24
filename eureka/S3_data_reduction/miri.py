80d64f6901d324bffe9a4fad6b676f2c71de0550
Taylor Bell <taylor.bell@mail.mcgill.ca>
2021-06-24 12:52:03 -0400

    Adding place-holder functions to miri.py.
    For now, I would guess that the NIRCam flag_bg() and fit_bg() functions will be instrument agnostic, so I just call those from miri.py for now.
    unit_convert() will likely have to be instrument dependent given that MIRI seems not to have data.mhdr['PUPIL']. Extra code will also
    likely be needed in the miri.read() function to rotate the MIRI data into the same orientation as NIRCam.

--- eureka/S3_data_reduction/miri.py
+++ eureka/S3_data_reduction/miri.py
@@ -4,6 +4,7 @@ import numpy as np
 from astropy.io import fits
 from eureka.S3_data_reduction import sigrej, optspex
 from . import bright2flux as b2f
+from . import nircam
 
 # Read FITS file from JWST's NIRCam instrument
 def read(filename, data):
@@ -43,5 +44,39 @@ def read(filename, data):
     data.wave    = hdulist['WAVELENGTH',1].data
     data.v0      = hdulist['VAR_RNOISE',1].data
     data.int_times = hdulist['INT_TIMES',1].data[data.intstart-1:data.intend]
-
+    
+    # MIRI seems to be rotated by 90° compared to NIRCam. There’s a “Dispersion Direction” note in the header.
+    # Sebastian would suggest inst (like miri.py) should check this value (it is axis=2 for MIRI and axis=1 for NIRCam)
+    # when the data is read in. If axis=2, transpose the data so that it works with the current setup of s3_reduce.py.
+    
     return data
+
+def unit_convert(data, meta, log):
+    '''
+    Temporary function template that will later convert from MJy/sr to e-
+    '''
+    
+    # currently: s3_reduce.py -> inst (like nircam.py) -> bright2flux.py -> bright2dn. But: data.mhdr['PUPIL'] doesn't
+    # exist in MIRI. Todo: Make that instrument-specific and make it work for miri. (Not really doable right now because
+    # the MIRI stage 2 calints files don't include wavelength information and you need that in order to correct for the
+    # wavelength-dependent response function).
+    
+    return data, meta
+
+def flag_bg(data, meta):
+    '''
+    Temporary function template that will later flag outliers in sky background along time axis
+    '''
+
+    # Code written for NIRCam and untested for MIRI, but likely to still work (as long as MIRI data gets rotated)
+    
+    return nircam.flag_bg(data, meta)
+
+def fit_bg(data, mask, y1, y2, bg_deg, p3thresh, n, isplots=False):
+    '''
+    Temporary function template that will later fit for non-uniform background
+    '''
+    
+    # Code written for NIRCam and untested for MIRI, but likely to still work (as long as MIRI data gets rotated)
+    
+    return nircam.fit_bg(data, mask, y1, y2, bg_deg, p3thresh, n, isplots)
