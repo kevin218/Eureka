d2aec57fd5d21adbd5ca5c1d89ea1ff73bd27684
Kevin B. Stevenson <kbstevenson@gmail.com>
2021-06-25 13:28:15 -0400

    Add S4 drift correction

--- eureka/S4_generate_lightcurves/plots_s4.py
+++ eureka/S4_generate_lightcurves/plots_s4.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def binned_lightcurve(meta, bjdtdb, i):
-    plt.figure(4100 + i, figsize=(8, 6))
+    plt.figure(4300 + i, figsize=(8, 6))
     plt.clf()
     plt.suptitle(f"Bandpass {i}: %.3f - %.3f" % (meta.wave_low[i], meta.wave_hi[i]))
     ax = plt.subplot(111)
@@ -18,4 +18,20 @@ def binned_lightcurve(meta, bjdtdb, i):
     plt.xlabel(f'Time [MJD + {mjd}]')
 
     plt.subplots_adjust(left=0.10, right=0.95, bottom=0.10, top=0.90, hspace=0.20, wspace=0.3)
-    plt.savefig(meta.lcdir + '/figs/Fig' + str(4100 + i) + '-' + meta.eventlabel + '-1D_LC.png')
+    plt.savefig(meta.lcdir + '/figs/Fig' + str(4300 + i) + '-' + meta.eventlabel + '-1D_LC.png')
+
+def drift1d(meta):
+    plt.figure(4101, figsize=(8,4))
+    plt.clf()
+    plt.plot(np.arange(meta.nx)[np.where(meta.driftmask)], meta.drift1d[np.where(meta.driftmask)], '.')
+    # plt.subplot(211)
+    # for j in range(istart,ev.n_reads-1):
+    #     plt.plot(ev.drift2D[:,j,1],'.')
+    # plt.ylabel('Spectrum Drift Along y')
+    # plt.subplot(212)
+    # for j in range(istart,ev.n_reads-1):
+    #     plt.plot(ev.drift2D[:,j,0]+ev.drift[:,j],'.')
+    plt.ylabel('Spectrum Drift Along x')
+    plt.xlabel('Frame Number')
+    plt.tight_layout()
+    plt.savefig(meta.lcdir + '/figs/Fig4101-Drift.png')
