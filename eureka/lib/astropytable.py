aebb36e1486675f95d41e5261bb9a6aba14d7fc2
Kevin B. Stevenson <kbstevenson@gmail.com>
2021-07-24 15:51:24 -0400

    Pass filename instead of meta to astropytable.savetable_S3

--- eureka/lib/astropytable.py
+++ eureka/lib/astropytable.py
@@ -4,7 +4,7 @@ from astropy.io import ascii
 import numpy as np
 import os
 
-def savetable_S3(meta, bjdtdb, wave_1d, stdspec, stdvar, optspec, opterr):
+def savetable_S3(filename, bjdtdb, wave_1d, stdspec, stdvar, optspec, opterr):
     """
       Saves data in an event as .txt using astropy
 
@@ -24,8 +24,6 @@ def savetable_S3(meta, bjdtdb, wave_1d, stdspec, stdvar, optspec, opterr):
       ---------
 
     """
-
-    filename = meta.workdir + '/S3_' + meta.eventlabel + "_Table_Save.txt"
 
     dims = stdspec.shape #tuple (integration, wavelength position)
 
