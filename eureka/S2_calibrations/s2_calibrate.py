b3a4253fe967dc3dbbe77a51e492d26ddae746c3
Taylor Bell <taylor.bell@mail.mcgill.ca>
2021-10-01 16:00:26 -0400

    Changed structure of s2_calibrate
    Working towards resolving Issue #60.
    
    s2_calibrate was trying to re-write much of Spec2Pipeline, so I've
    changed its structure to minimize duplicated code. Additionally,
    s2_calibrate was not saving the required calints and x1dints files,
    but now it will do so correctly. The outputs I get from x1dints for
    NIRSpec appear very poor quality and deserve further examination.
    The outputs for MIRI LRS seem good though.
    
    The current code is currently not in the eureka interaction format,
    and I will next work on making it more similar looking to S3.
    
    One other important issue is that Eureka S4 or onwards will need to be
    able to handle segmented FITS files since any observation larger than
    2GB will be segmented into smaller files.

--- eureka/S2_calibrations/s2_calibrate.py
+++ eureka/S2_calibrations/s2_calibrate.py
@@ -2,133 +2,81 @@
 
 # Eureka! Stage 2 calibration pipeline
 
-import os
+import os, sys
 import numpy as np
+import matplotlib.pyplot as plt
 from astropy.io import fits
 import jwst
-from jwst import datamodels, assign_wcs, extract_2d, srctype, photom, flatfield
+from jwst import datamodels
+from jwst.pipeline.calwebb_spec2 import Spec2Pipeline
 
-# class MetaClass:
-#     def __init__(self):
-#         # initialize Univ
-#         # Univ.__init__(self)
-#         # self.initpars(ecf)
-#         # self.foo = 2
-#         return
-# 
-#
-# class DataClass:
-#     def __init__(self):
-#         # initialize Univ
-#         # Univ.__init__(self)
-#         # self.initpars(ecf)
-#         # self.foo = 2
-#         return
+class EurekaS2Pipeline(Spec2Pipeline):
 
+  def run_eureka(self, filename, skip_assign_wcs=False, skip_extract_2d=False, skip_srctype=False,
+                 skip_flat_field=True, skip_photom=False, skip_extract_1d=False, output_dir=None, save_results=True):
 
-def calibrate_JWST(filename, do_assignwcs=True, do_extract2d=True, do_srctype=True, do_flatfield=False, do_photom=True, delete_files=True):
-	'''
-	Processes a Stage 1, *_rateints.fits to an equivalent of a Stage 2 *_calints.fits file, except the full subarray will be extracted instead of a trimmed 2D image.
-	Options to turn off and on certain steps are available, but are primarily for debugging
-	purposes - there is no guarantee that Eureka will work if you skip some of the steps.
+    if output_dir==None or len(output_dir)==0:
+      # If there was no output_dir provided, output into the same folder as the input files were in
+      output_dir = '/'.join(filename.split('/')[:-1])
+      if len(output_dir)>0:
+        # Only add a trailing slash if the input file wasn't in the current working directory
+        # (otherwise it'll try to save in the root directory)
+        output_dir += '/'
 
-	Parameters
-	----------
-	filename			: Single filename to read
-	do_assignwcs		: Boolean to perform the Assign WCS pipeline step
-	do_extract2d		: Boolean to perform the Extract 2D pipeline step
-	do_srctype			: Boolean to perform the Source Type pipeline step
-	do_flatfield		: Boolean to perform the Flat Field pipeline step
-	do_photom			: Boolean to perform the Photometric correction pipeline step
-	delete_files		: Boolean to delete intermediate files produced by the steps.
+    with fits.open(filename) as hdulist:
+      # Figure out which instrument we are using
+      inst = hdulist[0].header['INSTRUME']
+      if inst == 'NIRSPEC':
+        # Figure out what grating and filter we're using
+        # Needed to change the aperture used for NIRSpec outputs
+        grating = hdulist[0].header['GRATING']
+        filt = hdulist[0].header['FILTER']
 
-	Returns
-	-------
-	current_file		: Array of data frames
+    if inst == 'NIRSPEC' and grating == 'PRISM':
+      #Controls the cross-dispersion extraction
+      self.assign_wcs.slit_y_low = -1
+      self.assign_wcs.slit_y_high = 50
+      # Modify the existing file to broaden the dispersion extraction - FIX: DOES NOT WORK CURRENTLY
+      with datamodels.open(filename) as m:
+        #Control the dispersion extraction - FIX: DOES NOT WORK CURRENTLY
+        m.meta.wcsinfo.waverange_start = 6e-08
+        m.meta.wcsinfo.waverange_end = 6e-06
+        m.save(filename)
+    elif inst == 'NIRSPEC':
+      raise ValueError("I don't understand how to adjust the extraction aperture for this grating/filter yet!")
 
-	History
-	-------
-	Written by Aarynn Carter / Eva-Maria Ahrer  June 2021
-	'''
+    # FIX: This will likely overwritten by the cfg file later on
+    self.assign_wcs.skip = skip_assign_wcs
+    self.extract_2d.skip = skip_extract_2d
+    self.srctype.skip = skip_srctype
+    self.flat_field.skip = skip_flat_field
+    self.photom.skip = skip_photom
+    self.extract_1d.skip = skip_extract_1d
 
-	# Make sure we have the full directory location of the file
-	current_file = os.path.abspath(filename)
+    self.call(filename, output_dir=output_dir, save_results=save_results)
 
-	# Get the directory the data is saved in, and a prefix for the file itself.
-	file_dir = '/'.join(current_file.split('/')[:-1])
-	file_prefix = current_file.split('/')[-1].split('_rateints.fits')[0]
+    if save_results:
+      with datamodels.open(output_dir+'_'.join(filename.split('/')[-1].split('_')[:-1])+'_x1dints.fits') as sp1d:
+        fig, ax = plt.subplots(1,1, figsize=[15,5])
+        
+        for i in range(len(sp1d.spec)):
+            plt.plot(sp1d.spec[i].spec_table['WAVELENGTH'], sp1d.spec[i].spec_table['FLUX'])
 
-	# Grab location of config files
-	configs = jwst.__file__.split('__init__.py')[0] + 'pipeline/'
+        plt.title('Time Series Observation: Extracted spectra')
+        plt.xlabel('Wavelenth (micron)')
+        plt.ylabel('Flux')
+        plt.savefig(output_dir+'_'.join(filename.split('/')[-1].split('_')[:-1])+'_x1dints.png', bbox_inches='tight', dpi=300)
+        plt.close()
 
-	# Assign World Coordinate System - this is the step where we need to modify values to ensure the full subarray is extracted.
-	if do_assignwcs:
-		#Read in the *rateints.fits file so we know what grating/filter we are using.
-		with fits.open(current_file) as hdulist:
-			grating = hdulist[0].header['GRATING']
-			filt = hdulist[0].header['FILTER']
+def main(filename):
+  pipeline = EurekaS2Pipeline()
+  # FIX: Check ecf file to get skip booleans and enter them here
+  pipeline.run_eureka(filename)
 
-		# Set extraction values based on the grating/filter.
-		if grating == 'PRISM':
-			slit_y_low, slit_y_high = -1, 50 #Controls the cross-dispersion extraction
-			wav_start, wav_end = 6e-08, 6e-06   #Control the dispersion extraction - DOES NOT WORK CURRENTLY
-		else:
-			raise ValueError("I don't understand how to adjust the extraction aperture for this grating/filter yet!")
-
-		# Modify the existing file to broaden the dispersion extraction - DOES NOT WORK CURRENTLY
-		with datamodels.open(filename) as m:
-			m.meta.wcsinfo.waverange_start = wav_start
-			m.meta.wcsinfo.waverange_end = wav_end
-			m.save(filename)
-
-		# Run the step, note that the cross-dispersion extraction is input here.
-		stepname = 'assign_wcs'
-		curr_result = assign_wcs.AssignWcsStep.call(current_file, config_file=configs+stepname+'.cfg', slit_y_low=slit_y_low, slit_y_high=slit_y_high, output_dir=file_dir, output_file=file_prefix+'_{}.fits'.format(stepname))
-		# Update the current file we are working with.
-		current_file = file_dir + '/' + file_prefix+'_{}.fits'.format(stepname)
-
-	# Extract 2D spectrum, also does wavelength calibration
-	if do_extract2d:
-		stepname = 'extract_2d'
-		result = extract_2d.Extract2dStep.call(curr_result, config_file=configs+stepname+'.cfg', output_dir=file_dir, output_file=file_prefix+'_{}.fits'.format(stepname))
-		current_file = file_dir + '/' + file_prefix+'_{}.fits'.format(stepname)
-
-	# Identify source type
-	if do_srctype:
-		stepname = 'srctype'
-		result = srctype.SourceTypeStep.call(current_file, config_file=configs+stepname+'.cfg', output_dir=file_dir, output_file=file_prefix+'_{}.fits'.format(stepname))
-		current_file = file_dir + '/' + file_prefix+'_{}.fits'.format(stepname)
-
-	# Perform flat field correction
-	# ***NOTE*** At the time the NIRSpec ERS Hackathon simulated data was created, this step did not work correctly and is by default turned off.
-	if do_flatfield:
-		stepname = 'flat_field'
-		result = flatfield.FlatFieldStep.call(current_file, config_file=configs+stepname+'.cfg', output_dir=file_dir, output_file=file_prefix+'_{}.fits'.format(stepname))
-		current_file = file_dir + '/' + file_prefix+'_{}.fits'.format(stepname)
-
-	# Perform photometric correction to pixel count values.
-	if do_photom:
-		stepname = 'photom'
-		result = photom.PhotomStep.call(current_file, config_file=configs+stepname+'.cfg', output_dir=file_dir, output_file=file_prefix+'_{}.fits'.format(stepname))
-		current_file = file_dir + '/' + file_prefix+'_{}.fits'.format(stepname)
-
-	# Delete any intermediate files that were produced to keep things clean.
-	if delete_files:
-		to_remove = ['_assign_wcs.fits', '_extract_2d.fits', '_srctype.fits', '_flat_field.fits']
-		if do_photom:
-			to_remove = to_remove
-		elif do_flatfield:
-			to_remove = to_remove[:-1]
-		elif do_srctype:
-			to_remove = to_remove[:-2]
-		elif do_extract2d:
-			to_remove = to_remove[:-3]
-		else:
-			to_remove = []
-
-		for file_suffix in to_remove:
-			del_file = file_dir + '/' + file_prefix + file_suffix
-			if os.path.exists(del_file):
-				os.remove(del_file)
-
-	return current_file
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if len(args)==1:
+      filename = args
+    else:
+      raise ValueError('You must enter the science filename! (expected 1 arg, got {})'.format(len(args)))
+    main(filename)
