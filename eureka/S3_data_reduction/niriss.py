15353fd97c22f1e7048bd4697b321435457b645d
Adina Feinstein <adina.d.feinstein@gmail.com>
2021-10-01 19:02:56 -0400

    something about a data object?

--- eureka/S3_data_reduction/niriss.py
+++ eureka/S3_data_reduction/niriss.py
@@ -1,18 +1,62 @@
 # NIRISS specific rountines go here
 #
-#
 # Written by: Adina Feinstein
 # Last updated by: Adina Feinstein
 # Last updated date: October 1, 2021
 #
-#
+####################################
 
 import numpy as np
+from astropy.io import fits
 from skimage.morphology import disk
 from skimage import filters, feature
 from scipy.ndimage import gaussian_filter
 
-__all__ = ['create_niriss_mask']
+__all__ = ['read_niriss', 'create_niriss_mask']
+
+
+def read_niriss(filename, data, meta):
+    """
+    Reads a single FITS file from JWST's NIRISS instrument.
+    This takes in the Stage 2 processed files.
+
+    Parameters
+    ----------
+    filename : str
+       Single filename to read. Should be a `.fits` file.
+    data : object
+       Data object in which the fits data will be stored.
+
+    Returns
+    -------
+    data : object
+       Data object now populated with all of the FITS file
+       information.
+    meta : astropy.table.Table
+       Metadata stored in the FITS file.
+    """
+
+    assert(filename, str)
+
+    hdu = fits.open(filename)
+
+    # loads in all the header data
+    data.mhdr = hdu[0].header
+    data.shdr = hdu['SCI'][1].header
+
+    data.intend = hdu[0].header['NINTS'] + 0.0
+
+    # loads all the data into the data object
+    data.data = hdu['SCI',1].data
+    data.err  = hdu['ERR',1].data
+    data.dq   = hdu['DQ' ,1].data
+
+    data.var  = hdu['VAR_POISSON',1].data
+    data.v0   = hdu['VAR_RNOISE' ,1].data
+
+    meta = hdu['ASDF_METADATA',1].data
+
+    return data, meta
 
 
 def create_niriss_mask(imgs, anchors1=None, anchors2=None, plot=False):
@@ -91,7 +135,8 @@ def create_niriss_mask(imgs, anchors1=None, anchors2=None, plot=False):
     o1t = y_valid < l1(x_valid)
     o1b = (y_valid > l1(x_valid)) & (y_valid < l1(x_valid)+30)
 
-    o2t = (y_valid < l2(x_valid)) & (x_valid>750) & (y_valid > l1(x_valid)+29) & (y_valid <220)
+    o2t = ((y_valid < l2(x_valid)) & (x_valid>750) & 
+           (y_valid > l1(x_valid)+29) & (y_valid <220))
     o2b = (y_valid > l2(x_valid)) & (y_valid < l2(x_valid)+30) & (y_valid >90)
 
     masks = [o1t,o1b,o2t,o2b]
@@ -110,5 +155,16 @@ def create_niriss_mask(imgs, anchors1=None, anchors2=None, plot=False):
         img_mask[fits[0][i]:fits[0][i]+diff1,i] += 1
         img_mask[fits[2][i]:fits[2][i]+diff2,i] += 2
         
+        
+    # plots some of the intermediate and final steps
+    if plot:
+        fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, figsize=(14,8))
+        ax1.imshow(g)
+        ax1.set_title('Gaussian smoothed data')
+        ax2.imshow(z)
+        ax2.set_title('Canny edge detector')
+        ax3.imshow(img_mask, vmin=0, vmax=3)
+        ax3.set_title('Final mask')
+        plt.show()
 
     return img_mask
