129ac15ea6c9fc04381939c025e97db909448c6e
Taylor Bell <taylor.bell@mail.mcgill.ca>
2021-10-14 18:20:11 -0400

    Adding copyright rst file and adding S1-2 to index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,17 @@
 #
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
 project = 'Eureka!'
-copyright = '2021, Kevin Stevenson, Laura Kreidberg, Giannina Guzman, Sebastian Zieba'
-author = 'Kevin Stevenson, Laura Kreidberg, Giannina Guzman, Sebastian Zieba'
+copyright = '2021, Eureka! pipeline developers'
+author = 'Eureka! pipeline developers'
 
 # The full version, including alpha/beta/rc tags
 release = ''
@@ -62,7 +62,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 
 from recommonmark.parser import CommonMarkParser
---
+++ docs/source/copyright.rst
@@ -0,0 +1,14 @@
+Copyright
+==========
+
+.. include:: <isonum.txt>
+
+|copy| Copyright 2021, `Eureka! pipeline developers <https://github.com/kevin218/Eureka/graphs/contributors>`_
+
+The current main developers of Eureka! are:
+
+- Kevin Stevenson
+- Sebastian Zieba
+- Giannina Guzman
+- Laura Kreidberg
+- Taylor Bell
