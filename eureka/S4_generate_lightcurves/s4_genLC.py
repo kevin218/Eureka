Fixing S4 uncertainty calculation
There appears to be a bug in how the uncertainty in lightcurves is
calculated. Assuming Poisson noise, the error should be sqrt(N), but the
code seems to assume the noise level would be just N, so it ends up
being N^2 in the quadrature sum rather than just N.

eureka/S4_generate_lightcurves/s4_genLC.py
@@ -204,7 +204,7 @@ def lcJWST(eventlabel, s3_meta=None):
                 # Sum flux for each spectroscopic channel
                 meta.lcdata[i]    = np.sum(optspec[:,index],axis=1)
                 # Add uncertainties in quadrature
-                meta.lcerr[i]     = np.sqrt(np.sum(optspec[:,index]**2,axis=1))
+                meta.lcerr[i]     = np.sqrt(np.sum(optspec[:,index],axis=1))
 
                 # Plot each spectroscopic light curve
                 if meta.isplots_S4 >= 3: