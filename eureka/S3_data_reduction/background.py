611266466af499c2185a614f7968fd0191e162c9
Taylor Bell <taylor.bell@mail.mcgill.ca>
2021-11-08 17:38:02 -0500

    Adding back fitbg2 and now saving Fig6_BG plots
    fitbg2 will be helpful for NIRISS data which require more advanced
    masking of illuminated pixels given the higher orders present in the
    same field of view.

--- eureka/S3_data_reduction/background.py
+++ eureka/S3_data_reduction/background.py
@@ -50,13 +50,13 @@ def BGsubtraction(data, meta, log, isplots):
         # Only 1 CPU
         for n in tqdm(range(meta.int_start,n_int)):
             # Fit sky background with out-of-spectra data
-            writeBG(inst.fit_bg(subdata[n], submask[n], bg_y1, bg_y2, meta.bg_deg, meta.p3thresh, n, isplots))
+            writeBG(inst.fit_bg(subdata[n], meta, submask[n], bg_y1, bg_y2, meta.bg_deg, meta.p3thresh, n, isplots))
     else:
         # Multiple CPUs
         pool = mp.Pool(meta.ncpu)
         args_list = []
         for n in range(meta.int_start,n_int):
-            args_list.append((subdata[n], submask[n], bg_y1, bg_y2, meta.bg_deg, meta.p3thresh, n, isplots))
+            args_list.append((subdata[n], meta, submask[n], bg_y1, bg_y2, meta.bg_deg, meta.p3thresh, n, isplots))
         jobs = [pool.apply_async(func=inst.fit_bg, args=(*args,), callback=writeBG) for args in args_list]
         pool.close()
         for job in tqdm(jobs):
@@ -71,13 +71,15 @@ def BGsubtraction(data, meta, log, isplots):
     return data
 
 # STEP 3: Fit sky background with out-of-spectra data
-def fitbg(dataim, mask, x1, x2, deg=1, threshold=5, isrotate=False, isplots=0):
+def fitbg(dataim, meta, mask, x1, x2, deg=1, threshold=5, isrotate=False, isplots=0):
     '''Fit sky background with out-of-spectra data.
 
     Parameters
     ----------
     dataim: ndarray
         The data array
+    meta:   MetaClass
+        The metadata object.
     mask:   ndarray
         A mask array
     x1:     ndarray
@@ -93,9 +95,6 @@ def fitbg(dataim, mask, x1, x2, deg=1, threshold=5, isrotate=False, isplots=0):
     Notes
     ------
     History:
-
-    - September 2016 Kevin Stevenson
-        Initial version
 
     - May 2013
         Removed [::-1] for LDSS3
@@ -187,7 +186,7 @@ def fitbg(dataim, mask, x1, x2, deg=1, threshold=5, isrotate=False, isplots=0):
                     plt.title(str(j))
                     plt.plot(goodxvals, dataslice, 'bo')
                     plt.plot(range(nx), bg[j], 'g-')
-                    #plt.savefig('Fig6_BG_'+str(j)+'.png')
+                    plt.savefig(meta.outputdir + 'figs/Fig6_BG_'+str(j)+'.png')
                     plt.pause(0.01)
 
     if isrotate == 1:
@@ -198,3 +197,132 @@ def fitbg(dataim, mask, x1, x2, deg=1, threshold=5, isrotate=False, isplots=0):
         mask = (mask.T)
 
     return bg, mask
+
+# STEP 3: Fit sky background with out-of-spectra data
+def fitbg2(dataim, meta, mask, bgmask, deg=1, threshold=5, isrotate=False, isplots=False):
+    '''Fit sky background with out-of-spectra data.
+
+    fitbg2 uses bgmask, a mask for the background region which enables fitting more complex
+    background regions than simply above or below a given distance from the trace. This will
+    help mask the 2nd and 3rd orders of NIRISS.
+
+    Parameters
+    ----------
+    dataim: ndarray
+        The data array
+    meta:   MetaClass
+        The metadata object.
+    mask:   ndarray
+        A mask array
+    bgmask: ndarray
+        A background mask array.
+    deg:    int
+        Polynomial order for column-by-column background subtraction
+    threshold:  int
+        Sigma threshold for outlier rejection during background subtraction
+    isrotate:   bool
+    isplots:    int
+        The amount of plots saved; set in ecf.
+
+    Notes
+    ------
+    History:
+
+    - September 2016 Kevin Stevenson
+        Initial version
+    '''
+
+    # Assume x is the spatial direction and y is the wavelength direction
+    # Otherwise, rotate array
+    if isrotate == 1:
+        dataim = dataim[::-1].T
+        mask   = mask[::-1].T
+        bgmask = bgmask[::-1].T
+    elif isrotate == 2:
+        dataim = dataim.T
+        mask   = mask.T
+        bgmask = bgmask.T
+
+    # Initiate background image with zeros
+    ny, nx  = np.shape(dataim)
+    bg      = np.zeros((ny,nx))
+    mask2   = mask*bgmask
+    if deg < 0:
+        # Calculate median background of entire frame
+        bg  += np.median(dataim[np.where(mask2)])
+    elif deg == None :
+        # No background subtraction
+        pass
+    else:
+        degs = np.ones(ny)*deg
+        # Fit polynomial to each column
+        for j in range(ny):
+            nobadpixels = False
+            # Create x indices for background sections of frame
+            xvals   = np.where(bgmask[j] == 1)[0]
+            # If too few good pixels on either half of detector then compute average
+            if (np.sum(mask2[j,:nx/2]) < deg) or (np.sum(mask2[j,nx/2:nx]) < deg):
+                degs[j] = 0
+            while (nobadpixels == False):
+                try:
+                    goodxvals = xvals[np.where(mask[j,xvals])]
+                except:
+                    print(j)
+                    print(xvals)
+                    print(np.where(mask[j,xvals]))
+                    return
+                dataslice = dataim[j,goodxvals]
+                # Check for at least 1 good x value
+                if len(goodxvals) == 0:
+                    #print(j,ny)
+                    nobadpixels = True      #exit while loop
+                    #Use coefficients from previous row
+                else:
+                    # Fit along spatial direction with a polynomial of degree 'deg'
+                    coeffs    = np.polyfit(goodxvals, dataslice, deg=degs[j])
+                    # Evaluate model at goodexvals
+                    model     = np.polyval(coeffs, goodxvals)
+                    #model = smooth.smooth(dataslice, window_len=window_len, window=windowtype)
+                    #model = sps.medfilt(dataslice, window_len)
+                    if isplots == 6:
+                        plt.figure(3601)
+                        plt.clf()
+                        plt.title(str(j))
+                        plt.plot(goodxvals, dataslice, 'bo')
+                        plt.plot(goodxvals, model, 'g-')
+                        plt.savefig(meta.outputdir + 'figs/Fig6_BG_'+str(j)+'.png')
+                        plt.pause(0.01)
+
+                    # Calculate residuals
+                    residuals   = dataslice - model
+                    # Find worst data point
+                    loc         = np.argmax(np.abs(residuals))
+                    # Calculate standard deviation of points excluding worst point
+                    ind         = range(len(residuals))
+                    ind.remove(loc)
+                    stdres      = np.std(residuals[ind])
+                    if stdres == 0:
+                        stdres = np.inf
+                    # Calculate number of sigma from the model
+                    stdevs    = np.abs(residuals) / stdres
+                    # Mask data point if > threshold
+                    if stdevs[loc] > threshold:
+                        mask[j,goodxvals[loc]] = 0
+                    else:
+                        nobadpixels = True      #exit while loop
+
+            # Evaluate background model at all points, write model to background image
+            if len(goodxvals) != 0:
+                bg[j] = np.polyval(coeffs, range(nx))
+                #bg[j] = np.interp(range(nx), goodxvals, model)
+
+    if isrotate == 1:
+        bg      = (bg.T)[::-1]
+        mask    = (mask.T)[::-1]
+        bgmask  = (bgmask.T)[::-1]
+    elif isrotate == 2:
+        bg      = (bg.T)
+        mask    = (mask.T)
+        bgmask  = (bgmask.T)
+
+    return bg, mask #,variance
--- eureka/S3_data_reduction/nirspec.py
+++ eureka/S3_data_reduction/nirspec.py
@@ -95,9 +95,9 @@ def flag_bg(data, meta):
     return nircam.flag_bg(data, meta)
 
 
-def fit_bg(data, mask, y1, y2, bg_deg, p3thresh, n, isplots=False):
+def fit_bg(data, meta, mask, y1, y2, bg_deg, p3thresh, n, isplots=False):
     '''Fit for a non-uniform background.
 
     Uses the code written for NIRCam and untested for NIRSpec, but likely to still work
     '''
-    return nircam.fit_bg(data, mask, y1, y2, bg_deg, p3thresh, n, isplots=isplots)
+    return nircam.fit_bg(data, meta, mask, y1, y2, bg_deg, p3thresh, n, isplots=isplots)
