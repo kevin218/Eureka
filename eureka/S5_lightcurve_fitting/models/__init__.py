cfbdf92c665678db8e814d00f76c3abd863c6ced
Taylor Bell <bell@baeri.org>
2022-01-18 17:04:24 -0800

    Changing S5 models.py file to a directory
    This addresses Issue #90 but is as of yet untested

---
+++ eureka/S5_lightcurve_fitting/models/__init__.py
@@ -0,0 +1,5 @@
+from . import Model
+from .CompositeModel import CompositeModel
+from .TransitModel import TransitModel
+from .PolynomialModel import PolynomialModel
+from .ExpRampModel import ExpRampModel
