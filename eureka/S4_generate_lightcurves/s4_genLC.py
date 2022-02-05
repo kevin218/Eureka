9a497699fd72ed850a06eabe931bdf32dee58577
Taylor Bell <bell@baeri.org>
2022-02-04 17:16:10 -0800

    Significant edits to S4-5 to ensure correct inputs
    There was a big bug before where S4 and S5 would not actually use the
    outputs from many apertures and would only claim to do so. I have now
    fixed this and cleaned up the code with some new functions.

--- eureka/S4_generate_lightcurves/s4_genLC.py
+++ eureka/S4_generate_lightcurves/s4_genLC.py
@@ -66,88 +66,40 @@ def lcJWST(eventlabel, s3_meta=None):
     meta.eventlabel = eventlabel
 
     # Load Eureka! control file and store values in Event object
-    ecffile = 'S4_' + eventlabel + '.ecf'
+    ecffile = 'S4_' + meta.eventlabel + '.ecf'
     ecf = rd.read_ecf(ecffile)
     rd.store_ecf(meta, ecf)
 
-    #load savefile
     if s3_meta == None:
-        # Search for the S2 output metadata in the inputdir provided in
-        # First just check the specific inputdir folder
-        rootdir = os.path.join(meta.topdir, *meta.inputdir.split(os.sep))
-        if rootdir[-1]!='/':
-            rootdir += '/'
-        fnames = glob.glob(rootdir+'S3_'+meta.eventlabel+'*_Meta_Save.dat')
-        if len(fnames)==0:
-            # There were no metadata files in that folder, so let's see if there are in children folders
-            fnames = glob.glob(rootdir+'**/S3_'+meta.eventlabel+'*_Meta_Save.dat', recursive=True)
-            fnames = sn.sort_nicely(fnames)
+        #load savefile
+        s3_meta = read_s3_meta(meta)
 
-        if len(fnames)==0:
-            # There may be no metafiles in the inputdir - raise an error and give a helpful message
-            raise AssertionError('Unable to find an output metadata file from Eureka!\'s S3 step '
-                                +'in the inputdir: \n"{}"!'.format(rootdir))
-        elif len(fnames)>1:
-            # There may be multiple runs - use the most recent but warn the user
-            print('WARNING: There are multiple metadata save files in your inputdir: \n"{}"\n'.format(rootdir)
-                 +'Using the metadata file: \n{}\n'.format(fnames[-1])
-                 +'and will consider aperture ranges listed there. If this metadata file is not a part\n'
-                 +'of the run you intended, please provide a more precise folder for the metadata file.')
-
-        fname = fnames[-1] # Pick the last file name (should be the most recent or only file)
-        fname = fname[:-4] # Strip off the .dat ending
-
-        s3_meta = me.loadevent(fname)
-
-    # Need to remove the topdir from the outputdir
-    s3_outputdir = s3_meta.outputdir[len(s3_meta.topdir):]
-    if s3_outputdir[0]=='/':
-        s3_outputdir = s3_outputdir[1:]
-    if s3_outputdir[-1]!='/':
-        s3_outputdir += '/'
-
-    meta = s3_meta
-
-    # Load Eureka! control file and store values in the S3 metadata object
-    ecffile = 'S4_' + eventlabel + '.ecf'
-    ecf     = rd.read_ecf(ecffile)
-    rd.store_ecf(meta, ecf)
-
-    # Overwrite the inputdir with the exact output directory from S3
-    meta.inputdir = s3_outputdir
-    meta.old_datetime = meta.datetime # Capture the date that the
-    meta.datetime = None # Reset the datetime in case we're running this on a different day
-    meta.inputdir_raw = meta.inputdir
-    meta.outputdir_raw = meta.outputdir
+    meta = load_general_s3_meta_info(meta, s3_meta)
 
     if not meta.allapers:
         # The user indicated in the ecf that they only want to consider one aperture
         meta.spec_hw_range = [meta.spec_hw,]
         meta.bg_hw_range = [meta.bg_hw,]
 
+    # Create directories for Stage 5 outputs
     meta.runs_s4 = []
-    run_i = 0
     for spec_hw_val in meta.spec_hw_range:
+        for bg_hw_val in meta.bg_hw_range:
+            run = util.makedirectory(meta, 'S4', ap=spec_hw_val, bg=bg_hw_val)
+            meta.runs_s4.append(run)
 
+    run_i = 0
+    old_meta = meta
+    for spec_hw_val in meta.spec_hw_range:
         for bg_hw_val in meta.bg_hw_range:
 
             t0 = time.time()
 
-            meta.spec_hw = spec_hw_val
+            meta = load_specific_s3_meta_info(old_meta, run_i, spec_hw_val, bg_hw_val)
 
-            meta.bg_hw = bg_hw_val
-
-            # Do some folder swapping to be able to reuse this function to find S3 outputs
-            tempfolder = meta.outputdir_raw
-            meta.outputdir_raw = '/'.join(meta.inputdir_raw.split('/')[:-2])
-            meta.inputdir = util.pathdirectory(meta, 'S3', meta.runs[run_i], old_datetime=meta.old_datetime, ap=spec_hw_val, bg=bg_hw_val)
-            meta.outputdir_raw = tempfolder
+            # Get directory for Stage 4 processing outputs
+            meta.outputdir = util.pathdirectory(meta, 'S4', meta.runs_s4[run_i], ap=spec_hw_val, bg=bg_hw_val)
             run_i += 1
-
-            # Create directories for Stage 4 processing outputs
-            run = util.makedirectory(meta, 'S4', ap=spec_hw_val, bg=bg_hw_val)
-            meta.runs_s4.append(run)
-            meta.outputdir = util.pathdirectory(meta, 'S4', run, ap=spec_hw_val, bg=bg_hw_val)
 
             # Copy existing S3 log file and resume log
             meta.s4_logname  = meta.outputdir + 'S4_' + meta.eventlabel + ".log"
@@ -258,3 +210,87 @@ def lcJWST(eventlabel, s3_meta=None):
             log.closelog()
 
     return meta
+
+def read_s3_meta(meta):
+    
+    # Search for the S2 output metadata in the inputdir provided in
+    # First just check the specific inputdir folder
+    rootdir = os.path.join(meta.topdir, *meta.inputdir.split(os.sep))
+    if rootdir[-1]!='/':
+        rootdir += '/'
+    fnames = glob.glob(rootdir+'S3_'+meta.eventlabel+'*_Meta_Save.dat')
+    if len(fnames)==0:
+        # There were no metadata files in that folder, so let's see if there are in children folders
+        fnames = glob.glob(rootdir+'**/S3_'+meta.eventlabel+'*_Meta_Save.dat', recursive=True)
+        fnames = sn.sort_nicely(fnames)
+
+    if len(fnames)==0:
+        # There may be no metafiles in the inputdir - raise an error and give a helpful message
+        raise AssertionError('Unable to find an output metadata file from Eureka!\'s S3 step '
+                            +'in the inputdir: \n"{}"!'.format(rootdir))
+    elif len(fnames)>1:
+        # There may be multiple runs - use the most recent but warn the user
+        print('WARNING: There are multiple metadata save files in your inputdir: \n"{}"\n'.format(rootdir)
+                +'Using the metadata file: \n{}\n'.format(fnames[-1])
+                +'and will consider aperture ranges listed there. If this metadata file is not a part\n'
+                +'of the run you intended, please provide a more precise folder for the metadata file.')
+
+    fname = fnames[-1] # Pick the last file name (should be the most recent or only file)
+    fname = fname[:-4] # Strip off the .dat ending
+
+    s3_meta = me.loadevent(fname)
+
+    return s3_meta
+
+def load_general_s3_meta_info(meta, s3_meta):
+    # Need to remove the topdir from the outputdir
+    s3_outputdir = s3_meta.outputdir[len(s3_meta.topdir):]
+    if s3_outputdir[0]=='/':
+        s3_outputdir = s3_outputdir[1:]
+    if s3_outputdir[-1]!='/':
+        s3_outputdir += '/'
+
+    meta = s3_meta
+
+    # Load S4 Eureka! control file and store values in the S3 metadata object
+    ecffile = 'S4_' + meta.eventlabel + '.ecf'
+    ecf     = rd.read_ecf(ecffile)
+    rd.store_ecf(meta, ecf)
+
+    # Overwrite the inputdir with the exact output directory from S3
+    meta.inputdir = s3_outputdir
+    meta.old_datetime = meta.datetime # Capture the date that the
+    meta.datetime = None # Reset the datetime in case we're running this on a different day
+    meta.inputdir_raw = meta.inputdir
+    meta.outputdir_raw = meta.outputdir
+
+    return meta
+
+def load_specific_s3_meta_info(meta, run_i, spec_hw_val, bg_hw_val):
+    # Do some folder swapping to be able to reuse this function to find the correct S3 outputs
+    tempfolder = meta.outputdir_raw
+    meta.outputdir_raw = '/'.join(meta.inputdir_raw.split('/')[:-2])
+    meta.inputdir = util.pathdirectory(meta, 'S3', meta.runs[run_i], old_datetime=meta.old_datetime, ap=spec_hw_val, bg=bg_hw_val)
+    meta.outputdir_raw = tempfolder
+
+    # Read in the correct S3 metadata for this aperture pair
+    tempfolder = meta.inputdir
+    meta.inputdir = meta.inputdir[len(meta.topdir):]
+    new_meta = read_s3_meta(meta)
+    meta.inputdir = tempfolder
+
+    # Load S4 Eureka! control file and store values in the S3 metadata object
+    ecffile = 'S4_' + meta.eventlabel + '.ecf'
+    ecf     = rd.read_ecf(ecffile)
+    rd.store_ecf(new_meta, ecf)
+
+    # Save correctly identified folders from earlier
+    new_meta.inputdir = meta.inputdir
+    new_meta.outputdir = meta.outputdir
+    new_meta.inputdir_raw = meta.inputdir_raw
+    new_meta.outputdir_raw = meta.outputdir_raw
+
+    new_meta.runs_s4 = meta.runs_s4
+    new_meta.datetime = meta.datetime
+
+    return new_meta
