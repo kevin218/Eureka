acf83300374f824102fba7b4c8adc6f42d7f88e8
Taylor Bell <bell@baeri.org>
2022-02-16 13:28:22 -0800

    Finishing name changes

--- demos/S6/run_eureka.py
+++ demos/S6/run_eureka.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('../..')
-import eureka.S6_spectra_plotting as s6
+import eureka.S6_planet_spectra as s6
 
 eventlabel = 'template'
 
 if __name__ == '__main__':
-	s6_meta = s6.fitJWST(eventlabel)
+	s6_meta = s6.plot_spectra(eventlabel)
