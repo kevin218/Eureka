2d79a376a2fb6eba204f31e8c0f66d7c21a821d5
Taylor Bell <bell@baeri.org>
2022-02-17 11:00:36 -0800

    Moving S6 ecf template

--- demos/S6/run_eureka.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '../../')
-import eureka.S6_planet_spectra.s6_spectra as s6
-
-eventlabel = 'template'
-
-if __name__ == '__main__':
-	s6_meta = s6.plot_spectra(eventlabel)
