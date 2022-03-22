861ffd18173a743a4b3c9fb5cdd7a683fbf78cd6
Taylor Bell <bell@baeri.org>
2022-03-22 12:17:33 -0700

    Making new time array for newest MIRI simulations

--- eureka/S3_data_reduction/miri.py
+++ eureka/S3_data_reduction/miri.py
@@ -63,7 +63,10 @@ def read(filename, data, meta):
     if meta.firstFile:
         print('  WARNING: The timestamps for the simulated MIRI data are currently hardcoded '
               'because they are not in the .fits files themselves')
-    if data.mhdr['EFFINTTM']==10.3376:
+    if 'new_drift' in data.filename:
+        # Time array for the newest MIRISIM observations
+        data.time = np.linspace(0, 47.712*(1849)/3600/24, 1849, endpoint=True)[data.intstart - 1:data.intend-1]
+    elif data.mhdr['EFFINTTM']==10.3376:
         # There is no time information in the old simulated MIRI data
         # As a placeholder, I am creating timestamps indentical to the ones in STSci-SimDataJWST/MIRI/Ancillary_files/times.dat.txt converted to days
         data.time = np.linspace(0, 17356.28742796742/3600/24, 1680, endpoint=True)[data.intstart - 1:data.intend]
