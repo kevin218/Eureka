fdb8890d2ee71d3aa5490b6ddadf7aef41661350
Taylor Bell <bell@baeri.org>
2022-03-31 19:30:55 -0700

    Moving tests to main folder
    (where they are apparently supposed to be)

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This packages contains package tests.
+"""
+
+# import sys
+# sys.path.insert(0, './')
+# import test_general
+# import test_lightcurve_fitting
