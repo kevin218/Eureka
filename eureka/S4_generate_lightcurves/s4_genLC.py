0003957c0f74dfe0f1bfe0b0e16bc514e1f56c0a
Aarynn Carter <23636747+AarynnCarter@users.noreply.github.com>
2022-04-14 17:07:29 -0700

    remove print statement used for debugging

--- eureka/S4_generate_lightcurves/s4_genLC.py
+++ eureka/S4_generate_lightcurves/s4_genLC.py
@@ -238,8 +238,6 @@ def read_s3_meta(meta):
     if rootdir[-1]!='/':
         rootdir += '/'
     fnames = glob.glob(rootdir+'S3_'+meta.eventlabel+'*_Meta_Save.dat')
-    print(rootdir+'S3_'+meta.eventlabel+'*_Meta_Save.dat')
-    print(fnames)
     if len(fnames)==0:
         # There were no metadata files in that folder, so let's see if there are in children folders
         fnames = glob.glob(rootdir+'**/S3_'+meta.eventlabel+'*_Meta_Save.dat', recursive=True)
