Fixing masking issues with S4 plots

eureka/S4_generate_lightcurves/plots_s4.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
+from ..lib import util
 
-
-def binned_lightcurve(meta, time, i):
+def binned_lightcurve(meta, time, wavelength, i):
     '''Plot each spectroscopic light curve. (Fig 4300)
 
     Parameters
@@ -11,6 +12,8 @@ def binned_lightcurve(meta, time, i):
         The metadata object.
     time:  ndarray (1D)
         The time in meta.time_units of each data point.
+    wavelength: ndarray (1D)
+        The wavelength array
     i:  int
         The current bandpass number.
 
@@ -22,18 +25,18 @@ def binned_lightcurve(meta, time, i):
     plt.clf()
     plt.suptitle(f"Bandpass {i}: %.3f - %.3f" % (meta.wave_low[i], meta.wave_hi[i]))
     ax = plt.subplot(111)
-    time_modifier = np.floor(time[0])
+    time_modifier = np.ma.floor(time[0])
     # Normalized light curve
-    norm_lcdata = meta.lcdata[i] / meta.lcdata[i, -1]
-    norm_lcerr = meta.lcerr[i] / meta.lcdata[i, -1]
+    norm_lcdata = meta.lcdata[i] / np.ma.mean(meta.lcdata[i,:])
+    norm_lcerr = meta.lcerr[i] / np.ma.mean(meta.lcdata[i,:])
     plt.errorbar(time - time_modifier, norm_lcdata, norm_lcerr, fmt='o', color=f'C{i}', mec=f'C{i}', alpha = 0.2)
-    plt.text(0.05, 0.1, "MAD = " + str(np.round(1e6 * np.ma.median(np.abs(np.ediff1d(norm_lcdata))))) + " ppm",
-             transform=ax.transAxes, color='k')
+    mad = 1e6 * np.ma.median(np.ma.abs(np.ma.ediff1d(norm_lcdata)))
+    plt.text(0.05, 0.1, f"MAD = {np.round(mad).astype(int)} ppm", transform=ax.transAxes, color='k')
     plt.ylabel('Normalized Flux')
     plt.xlabel(f'Time [{meta.time_units} - {time_modifier}]')
 
     plt.subplots_adjust(left=0.10, right=0.95, bottom=0.10, top=0.90, hspace=0.20, wspace=0.3)
-    plt.savefig(meta.outputdir + 'figs/Fig43{}-1D_LC.png'.format(str(i).zfill(int(np.floor(np.log10(meta.nspecchan))+1))))
+    plt.savefig(meta.outputdir + 'figs/Fig43{}-1D_LC.png'.format(str(i).zfill(int(np.floor(np.log10(meta.nspecchan))+1))), bbox_inches='tight')
     if not meta.hide_plots:
         plt.pause(0.2)
 
@@ -55,7 +58,7 @@ def drift1d(meta):
     plt.ylabel('Spectrum Drift Along x')
     plt.xlabel('Frame Number')
     plt.tight_layout()
-    plt.savefig(meta.outputdir + 'figs/Fig41{}-Drift.png'.format(str(0).zfill(int(np.floor(np.log10(meta.nspecchan))+1))))
+    plt.savefig(meta.outputdir + 'figs/Fig41{}-Drift.png'.format(str(0).zfill(int(np.floor(np.log10(meta.nspecchan))+1))), bbox_inches='tight')
     if not meta.hide_plots:
         plt.pause(0.2)
 
@@ -77,26 +80,26 @@ def lc_driftcorr(meta, wave_1d, optspec):
     '''
     plt.figure(int('42{}'.format(str(0).zfill(int(np.floor(np.log10(meta.nspecchan))+1)))), figsize=(8, 8))
     plt.clf()
-    wmin = wave_1d.min()
-    wmax = wave_1d.max()
+    wmin = np.ma.min(wave_1d)
+    wmax = np.ma.max(wave_1d)
     n_int, nx = optspec.shape
     vmin = 0.97
     vmax = 1.03
-    normspec = optspec / np.mean(optspec, axis=0)
+    normspec = optspec / np.ma.mean(optspec, axis=0)
     plt.imshow(normspec, origin='lower', aspect='auto', extent=[wmin, wmax, 0, n_int], vmin=vmin, vmax=vmax,
                cmap=plt.cm.RdYlBu_r)
-    plt.title("MAD = " + str(np.round(meta.mad_s4, 0).astype(int)) + " ppm")
+    plt.title("MAD = " + str(np.round(meta.mad_s4).astype(int)) + " ppm")
     if meta.nspecchan > 1:
         # Insert vertical dashed lines at spectroscopic channel edges
         secax = plt.gca().secondary_xaxis('top')
         xticks = np.unique(np.concatenate([meta.wave_low,meta.wave_hi]))
-        secax.set_xticks(xticks, xticks, rotation=90)
+        secax.set_xticks(xticks, np.round(xticks, 6), rotation=90, fontsize=rcParams['font.size']*0.5)
         plt.vlines(xticks,0,n_int,'0.3','dashed')
     plt.ylabel('Integration Number')
     plt.xlabel(r'Wavelength ($\mu m$)')
     plt.colorbar(label='Normalized Flux')
     plt.tight_layout()
-    plt.savefig(meta.outputdir + 'figs/Fig42{}-2D_LC.png'.format(str(0).zfill(int(np.floor(np.log10(meta.nspecchan))+1))))
+    plt.savefig(meta.outputdir + 'figs/Fig42{}-2D_LC.png'.format(str(0).zfill(int(np.floor(np.log10(meta.nspecchan))+1))), bbox_inches='tight')
     if meta.hide_plots:
         plt.close()
     else:
@@ -121,15 +124,15 @@ def cc_spec(meta, ref_spec, fit_spec, n):
     -------
     None
     '''
-    plt.figure(int('44{}'.format(str(0).zfill(int(np.floor(np.log10(meta.nspecchan))+1)))), figsize=(8, 8))
+    plt.figure(int('44{}'.format(str(0).zfill(int(np.floor(np.log10(meta.n_int))+1)))), figsize=(8, 8))
     plt.clf()
     plt.title(f'Cross Correlation - Spectrum {n}')
     nx = len(ref_spec)
-    plt.plot(range(nx), ref_spec, '-', label='Reference Spectrum')
-    plt.plot(range(meta.drift_range,nx-meta.drift_range), fit_spec, '-', label='Current Spectrum')
+    plt.plot(np.arange(nx), ref_spec, '-', label='Reference Spectrum')
+    plt.plot(np.arange(meta.drift_range,nx-meta.drift_range), fit_spec, '-', label='Current Spectrum')
     plt.legend(loc='best')
     plt.tight_layout()
-    plt.savefig(meta.outputdir + 'figs/Fig44{}-CC_Spec.png'.format(str(n).zfill(int(np.floor(np.log10(meta.nspecchan))+1))))
+    plt.savefig(meta.outputdir + 'figs/Fig44{}-CC_Spec.png'.format(str(n).zfill(int(np.floor(np.log10(meta.n_int))+1))), bbox_inches='tight')
     if not meta.hide_plots:
         plt.pause(0.2)
 
@@ -149,11 +152,11 @@ def cc_vals(meta, vals, n):
     -------
     None
     '''
-    plt.figure(int('45{}'.format(str(0).zfill(int(np.floor(np.log10(meta.nspecchan))+1)))), figsize=(8, 8))
+    plt.figure(int('45{}'.format(str(0).zfill(int(np.floor(np.log10(meta.n_int))+1)))), figsize=(8, 8))
     plt.clf()
     plt.title(f'Cross Correlation - Values {n}')
-    plt.plot(range(-meta.drift_range,meta.drift_range+1), vals, '.')
+    plt.plot(np.arange(-meta.drift_range,meta.drift_range+1), vals, '.')
     plt.tight_layout()
-    plt.savefig(meta.outputdir + 'figs/Fig45{}-CC_Vals.png'.format(str(n).zfill(int(np.floor(np.log10(meta.nspecchan))+1))))
+    plt.savefig(meta.outputdir + 'figs/Fig45{}-CC_Vals.png'.format(str(n).zfill(int(np.floor(np.log10(meta.n_int))+1))), bbox_inches='tight')
     if not meta.hide_plots:
         plt.pause(0.2)