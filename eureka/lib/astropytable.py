7b28faff35b97e79ee5d6437c5543794732fb2f2
cpiaulet <caroline.piaulet@umontreal.ca>
2022-04-25 15:14:42 -0400

    minor fixes

--- eureka/lib/astropytable.py
+++ eureka/lib/astropytable.py
@@ -97,7 +97,6 @@ def savetable_S5(filename, time, wavelength, bin_width, lcdata, lcerr, model, re
     Revisions
     ---------
     """
-
     dims = [len(time), len(wavelength)]
 
     orig_shapes = [str(time.shape), str(wavelength.shape), str(bin_width.shape), str(lcdata.shape), str(lcerr.shape), str(model.shape), str(residuals.shape)]
