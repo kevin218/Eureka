4499cb19107b2161c8e21236b997f10d7a3d4751
Adina Feinstein <adina.d.feinstein@gmail.com>
2022-04-25 22:39:23 -0500

    minor formatting changes

--- eureka/S3_data_reduction/niriss_cython.pyx
+++ eureka/S3_data_reduction/niriss_cython.pyx
@@ -4,11 +4,11 @@ __all__ = ['build_gaussian_images', 'build_moffat_images']
 
 def build_gaussian_images(data, A, B, sig, mu1, mu2, return_together=True):
 
-    y = np.transpose(np.full((len(A), data.shape[-1], data.shape[0]), 
+    y = np.transpose(np.full((len(A), data.shape[-1], data.shape[0]),
                          np.arange(0,data.shape[0],1)), axes=(0,2,1))
 			 
     sig = np.full(y.T.shape,sig).T
-    
+
     A = np.full(y.T.shape, A).T
     exp = np.exp(-(y-mu1)**2/(2.0 * sig**2))
     gauss = 1.0/(2.0*np.pi*np.sqrt(sig)) * exp
@@ -33,19 +33,17 @@ def build_moffat_images(data, A, alpha, gamma, mu1, mu2, return_together=True):
        frac = 1 + (y - x)**2.0 / gamma**2.0
        return A * frac**(-alpha)
 
-    y = np.transpose(np.full((len(alpha), data.shape[-1], data.shape[0]),	
+    y = np.transpose(np.full((len(alpha), data.shape[-1], data.shape[0]),
                          np.arange(0,data.shape[0],1)), axes=(0,2,1))
-    
+
     A = np.full(y.T.shape, A).T
     alpha = np.full(y.T.shape, alpha).T
     gamma = np.full(y.T.shape, gamma).T
 
     m1 = moffat(mu1, A, alpha, gamma)
     m2 = moffat(mu2, A, alpha, gamma)
-    
+
     if return_together:
        return m1 + m2
     else:
        return m1, m2
-
-			 
