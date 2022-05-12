7db29702eca0b4627ce39286ab38680a74f858cf
Taylor Bell <bell@baeri.org>
2022-05-12 15:14:42 -0700

    Fixing issues with missing meta.tab_filename

--- eureka/S4_generate_lightcurves/s4_genLC.py
+++ eureka/S4_generate_lightcurves/s4_genLC.py
@@ -119,7 +119,7 @@ def genlc(eventlabel, ecf_path=None, s3_meta=None):
             log.writelog('Copying S4 control file', mute=(not meta.verbose))
             meta.copy_ecf()
 
-            log.writelog(f"Loading S3 save file:\n{meta.tab_filename}", mute=(not meta.verbose))
+            log.writelog(f"Loading S3 save file:\n{meta.filename_S3_SpecData}", mute=(not meta.verbose))
             spec = xrio.readXR(meta.filename_S3_SpecData)
 
             if meta.wave_min is None:
@@ -270,9 +270,7 @@ def load_specific_s3_meta_info(meta):
     # Locate the old MetaClass savefile, and load new ECF into that old MetaClass
     meta.inputdir = inputdir
     s3_meta, meta.inputdir, meta.inputdir_raw = me.findevent(meta, 'S3', allowFail=False)
-    tab_filename = s3_meta.tab_filename
     # Merge S4 meta into old S3 meta
     meta = me.mergeevents(meta, s3_meta)
-    meta.tab_filename = tab_filename
 
     return meta
