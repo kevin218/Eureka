73f5e37a0bd4f3dc4ec71cf31de6e56fd86745ee
Adina Feinstein <adina.d.feinstein@gmail.com>
2022-06-07 10:04:48 -0500

    better naming of trace tables (rather than tab1/tab2)

--- eureka/S3_data_reduction/niriss.py
+++ eureka/S3_data_reduction/niriss.py
@@ -152,7 +152,7 @@ def fit_bg(data, meta, log,
     data : object
     bkg : np.ndarray
     """
-    box_mask = dirty_mask(data.medflux.values, meta.tab1, booltype=True,
+    box_mask = dirty_mask(data.medflux.values, meta.trace1, booltype=True,
                           return_together=True)
     bkg, bkg_var, cr_mask = fitbg3(data, np.array(box_mask-1, dtype=bool),
                                    readnoise, sigclip, bkg_estimator=bkg_estimator,
