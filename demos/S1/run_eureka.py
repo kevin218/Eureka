c1d8a76811b4d92049da205e6fcfe8503a94da03
evamariaa <evamaria.ahrer@gmail.com>
2022-06-13 18:31:43 +0100

    Revert "Merge branch 'main' into dev/ea"
    This reverts commit 7e67b8dbee04548bf84390ca8f2f26d4b44483ba, reversing
    changes made to e6e5f839d026a4f0c6c0a932fec6a595a8a269c5.

--- demos/S1/run_eureka.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('../..')
-from eureka.S1_detector_processing.s1_process import EurekaS1Pipeline
-
-eventlabel = 'nirx_template'
-# eventlabel = 'miri_template'
-
-s1 = EurekaS1Pipeline()
-ev2 = s1.run_eurekaS1(eventlabel)
