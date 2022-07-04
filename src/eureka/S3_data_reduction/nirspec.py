86f8a8aa14533e89bf3c034eed5fd14cd75b8456
paroy <bbenneke@gmail.com>
2022-07-04 10:56:09 -0400

    clean and update documentation

--- src/eureka/S3_data_reduction/nirspec.py
+++ src/eureka/S3_data_reduction/nirspec.py
@@ -157,68 +157,23 @@ def fit_bg(dataim, datamask, n, meta, isplots=0):
     return nircam.fit_bg(dataim, datamask, n, meta, isplots=isplots)
 
 
-def find_column_coms_and_shifts(data):
-    '''
-    Takes a data array (int, rows, columns) and find the 
-    center of mass (COM) in pixels for each column. It then returns the needed
-    shift to apply to each column to bring the COM to the center
-
-    Input:
-    data : (array) sequence of images. Shape: (Nb_integrations x Nb_rows x Nb_columns)
-
-    Return:
-    shifts : (array) shift to apply to each column of each integration to move
-                the trace to the middle of the detector and make it straight
-                Shape: (Nb_integrations x Nb_columns)
-
-
-    '''
-
-    # get the dimensions of the data
-    nb_rows = data.shape[1]
-
-    # define a array of pixel positions (in their centers)
-    pix_centers = np.arange(nb_rows) + 0.5
-
-    # Compute the center of mass of each column and convert to integer (pixels)
-    column_coms = np.sum(pix_centers[None, :, None]*data, axis=1) / np.sum(data, axis=1)
-    column_coms = np.around(column_coms).astype(int)
-
-    # # try using the max of each column
-    # column_max = np.argmax(data, axis=1)
-    # column_coms = column_max.astype(int)
-
-    # define the new center (where we will align the trace) in the middle of the detector
-    new_center = int(nb_rows/2)
-
-    # define an array containing the needed shift to bring the COMs to the
-    # new center for each column of each integration
-    shifts = new_center - column_coms
-
-    # ensure columns of zeros or nans are not moved and dont induce bugs
-    shifts[column_coms < 0] = 0
-    shifts[column_coms > nb_rows] = 0
-
-    return shifts, new_center
-
-
 def find_column_median_shifts(data):
-    '''
-    Takes the median frame (in time) and finds the 
+    '''Takes the median frame (in time) and finds the 
     center of mass (COM) in pixels for each column. It then returns the needed
     shift to apply to each column to bring the COM to the center
 
-    Input:
-    data : (array) median flux in time. Shape: (Nb_rows x Nb_columns)
+    Parameters
+    ----------
+    data : ndarray (2D)
+        The median of all data frames
 
-    Return:
-    shifts : (array) shift to apply to each column to move
-                the trace to the middle of the detector and make it straight
-                Shape: (Nb_columns)
-
-
+    Returns
+    -------
+    shifts : ndarray (2D)
+        The shifts to apply to each column to straighten the trace.
+    new_center : int
+        The central row of the detector where the trace is moved to.
     '''
-
     # get the dimensions of the data
     nb_rows = data.shape[0]
 
@@ -228,10 +183,6 @@ def find_column_median_shifts(data):
     # Compute the center of mass of each column and convert to integer (pixels)
     column_coms = np.sum(pix_centers[:, None]*data, axis=0) / np.sum(data, axis=0)
     column_coms = np.around(column_coms).astype(int)
-
-    # # try using the max of each column
-    # column_max = np.argmax(data, axis=1)
-    # column_coms = column_max.astype(int)
 
     # define the new center (where we will align the trace) in the middle of the detector
     new_center = int(nb_rows/2)
@@ -248,21 +199,21 @@ def find_column_median_shifts(data):
 
 
 def roll_columns(data, shifts):
-    '''
-    For each column of each integration, rolls the columns by the 
+    '''For each column of each integration, rolls the columns by the 
     values specified in shifts 
 
-    Input:
-    data   : (array) sequence of images. Shape: (Nb_integrations x Nb_rows x Nb_cols)
-    shifts : (array) shift to apply to each column of each integration
-                        Shape: (Nb_integrations x Nb_columns)
+    Parameters
+    ----------
+    data : ndarray (3D)
+        Image data.
+    shifts : ndarray (2D)
+        The shifts to apply to each column to straighten the trace.
 
-    Return:
-    rolled_data : (array) sequence of images with same shape as data but 
-                            with the shifts applied
-
+    Returns
+    -------
+    rolled_data : ndarray (3D)
+        Image data with the shifts applied.
     '''
-
     # init straight_data
     rolled_data = np.zeros_like(data)
     # loop over all images (integrations)
@@ -288,8 +239,7 @@ def roll_columns(data, shifts):
     
 
 def straighten_trace(data, meta, log):
-    '''
-    Takes a set of integrations with a curved trace and shifts the 
+    '''Takes a set of integrations with a curved trace and shifts the 
     columns to bring the center of mass to the middle of the detector
     (and straighten the trace)
 
@@ -297,24 +247,27 @@ def straighten_trace(data, meta, log):
     The shifts to be applied are computed once from the median frame and then
     applied to each integration in the timeseries
 
-    Inputs:
-    =======
-    data : Eureka! data object (Astraeus object)
-    meta : Eureka! meta object
+    Parameters
+    ----------
+    data : Xarray Dataset
+        The Dataset object in which the fits data will stored.
+    meta : eureka.lib.readECF.MetaClass
+        The metadata object.
+    log : logedit.Logedit
+        The open log in which notes from this step can be added.
 
-    Returns:
-    ========
-    data : Eureka! data object with the trace correction
-            Updates: (flux, err, dq, v0, medflux, wave_2d, wave_1d)
-    meta : Eureka! meta object with the src_ypos argument updated
-
+    Returns
+    -------
+    data : Xarray Dataset
+        The updated Dataset object with the fits data stored inside.
+    meta : eureka.lib.readECF.MetaClass
+        The updated metadata object.
     '''
-
     log.writelog('  Correcting curvature and bringing trace in the center '
-    			 'of the detector', mute=(not meta.verbose))
+                    'of the detector', mute=(not meta.verbose))
     # This method only works with the median profile for the extraction
     log.writelog('  !!! Ensure that you are using meddata for the optimal '
-    			 'extraction profile !!!', mute=(not meta.verbose))
+                    'extraction profile !!!', mute=(not meta.verbose))
 
     # Find the median shift needed to bring the trace centered on the detector
     # obtain the median frame
@@ -344,12 +297,12 @@ def straighten_trace(data, meta, log):
     
     # update the new src_ypos
     log.writelog('  Update src_ypos to new center, row {}'.format(new_center),
-    				mute=(not meta.verbose))
+                mute=(not meta.verbose))
     meta.src_ypos = new_center
 
     # update the median frame
     log.writelog('  Update median frame now that the trace is corrected',
-    				mute=(not meta.verbose))
+                mute=(not meta.verbose))
     data.medflux.values = np.median(data.flux.values, axis=0)
 
     return data, meta
