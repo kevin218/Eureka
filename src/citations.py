46b90642f14403ce35c14d8ca0eb0633668ba30e
Yoni Brande <rocketfreak@gmail.com>
2022-07-21 18:57:46 -0500

    added extra basic citations

--- src/citations.py
+++ src/citations.py
@@ -1,7 +1,7 @@
 CITATIONS = {
     "eureka": (
         ("eureka"), 
-        r"""@ARTICLE{eureka,
+        [r"""@ARTICLE{eureka,
                 author = {{Bell}, Taylor J. and {Ahrer}, Eva-Maria and {Brande}, Jonathan and {Carter}, Aarynn L. and 
                 {Feinstein}, Adina D. and {Guzman Caloca}, Giannina and {Mansfield}, Megan and {Zieba}, Sebastian and 
                 {Piaulet}, Caroline and {Benneke}, Bj{\"o}rn and {Filippazzo}, Joseph and {May}, Erin M. and 
@@ -18,6 +18,155 @@ CITATIONS = {
             primaryClass = {astro-ph.IM},
                 adsurl = {https://ui.adsabs.harvard.edu/abs/2022arXiv220703585B},
                 adsnote = {Provided by the SAO/NASA Astrophysics Data System}
-            }"""
+            }"""]
+    ),
+    "numpy": (
+        ("numpy"),
+        [r"""@Article{numpy,
+            title         = {Array programming with {NumPy}},
+            author        = {Charles R. Harris and K. Jarrod Millman and St{\'{e}}fan J.
+                            van der Walt and Ralf Gommers and Pauli Virtanen and David
+                            Cournapeau and Eric Wieser and Julian Taylor and Sebastian
+                            Berg and Nathaniel J. Smith and Robert Kern and Matti Picus
+                            and Stephan Hoyer and Marten H. van Kerkwijk and Matthew
+                            Brett and Allan Haldane and Jaime Fern{\'{a}}ndez del
+                            R{\'{i}}o and Mark Wiebe and Pearu Peterson and Pierre
+                            G{\'{e}}rard-Marchant and Kevin Sheppard and Tyler Reddy and
+                            Warren Weckesser and Hameer Abbasi and Christoph Gohlke and
+                            Travis E. Oliphant},
+            year          = {2020},
+            month         = sep,
+            journal       = {Nature},
+            volume        = {585},
+            number        = {7825},
+            pages         = {357--362},
+            doi           = {10.1038/s41586-020-2649-2},
+            publisher     = {Springer Science and Business Media {LLC}},
+            url           = {https://doi.org/10.1038/s41586-020-2649-2}
+        }"""]
+    ),
+    "matplotlib": (
+        ("matplotlib"),
+        [r"""@Article{matplotlib,
+            Author    = {Hunter, J. D.},
+            Title     = {Matplotlib: A 2D graphics environment},
+            Journal   = {Computing in Science \& Engineering},
+            Volume    = {9},
+            Number    = {3},
+            Pages     = {90--95},
+            abstract  = {Matplotlib is a 2D graphics package used for Python for
+            application development, interactive scripting, and publication-quality
+            image generation across user interfaces and operating systems.},
+            publisher = {IEEE COMPUTER SOC},
+            doi       = {10.1109/MCSE.2007.55},
+            year      = 2007
+            }"""]
+    ),
+    "scipy": (
+        ("scipy"),
+        [r"""@ARTICLE{scipy,
+            author  = {Virtanen, Pauli and Gommers, Ralf and Oliphant, Travis E. and
+                        Haberland, Matt and Reddy, Tyler and Cournapeau, David and
+                        Burovski, Evgeni and Peterson, Pearu and Weckesser, Warren and
+                        Bright, Jonathan and {van der Walt}, St{\'e}fan J. and
+                        Brett, Matthew and Wilson, Joshua and Millman, K. Jarrod and
+                        Mayorov, Nikolay and Nelson, Andrew R. J. and Jones, Eric and
+                        Kern, Robert and Larson, Eric and Carey, C J and
+                        Polat, {\.I}lhan and Feng, Yu and Moore, Eric W. and
+                        {VanderPlas}, Jake and Laxalde, Denis and Perktold, Josef and
+                        Cimrman, Robert and Henriksen, Ian and Quintero, E. A. and
+                        Harris, Charles R. and Archibald, Anne M. and
+                        Ribeiro, Ant{\^o}nio H. and Pedregosa, Fabian and
+                        {van Mulbregt}, Paul and {SciPy 1.0 Contributors}},
+            title   = {{{SciPy} 1.0: Fundamental Algorithms for Scientific
+                        Computing in Python}},
+            journal = {Nature Methods},
+            year    = {2020},
+            volume  = {17},
+            pages   = {261--272},
+            adsurl  = {https://rdcu.be/b08Wh},
+            doi     = {10.1038/s41592-019-0686-2},
+        }"""]
+    ),
+    "astropy": (
+        ("astropy"),
+        [r"""@article{astropy2013,
+            Adsnote = {Provided by the SAO/NASA Astrophysics Data System},
+            Adsurl = {http://adsabs.harvard.edu/abs/2013A%26A...558A..33A},
+            Archiveprefix = {arXiv},
+            Author = {{Astropy Collaboration} and {Robitaille}, T.~P. and {Tollerud}, E.~J. and {Greenfield}, P. and {Droettboom}, M. and {Bray}, E. and {Aldcroft}, T. and {Davis}, M. and {Ginsburg}, A. and {Price-Whelan}, A.~M. and {Kerzendorf}, W.~E. and {Conley}, A. and {Crighton}, N. and {Barbary}, K. and {Muna}, D. and {Ferguson}, H. and {Grollier}, F. and {Parikh}, M.~M. and {Nair}, P.~H. and {Unther}, H.~M. and {Deil}, C. and {Woillez}, J. and {Conseil}, S. and {Kramer}, R. and {Turner}, J.~E.~H. and {Singer}, L. and {Fox}, R. and {Weaver}, B.~A. and {Zabalza}, V. and {Edwards}, Z.~I. and {Azalee Bostroem}, K. and {Burke}, D.~J. and {Casey}, A.~R. and {Crawford}, S.~M. and {Dencheva}, N. and {Ely}, J. and {Jenness}, T. and {Labrie}, K. and {Lim}, P.~L. and {Pierfederici}, F. and {Pontzen}, A. and {Ptak}, A. and {Refsdal}, B. and {Servillat}, M. and {Streicher}, O.},
+            Doi = {10.1051/0004-6361/201322068},
+            Eid = {A33},
+            Eprint = {1307.6212},
+            Journal = {\aap},
+            Keywords = {methods: data analysis, methods: miscellaneous, virtual observatory tools},
+            Month = oct,
+            Pages = {A33},
+            Primaryclass = {astro-ph.IM},
+            Title = {{Astropy: A community Python package for astronomy}},
+            Volume = 558,
+            Year = 2013,
+            Bdsk-Url-1 = {https://dx.doi.org/10.1051/0004-6361/201322068}
+        }""",
+        r"""@ARTICLE{astropy2018,
+            author = {{Astropy Collaboration} and {Price-Whelan}, A.~M. and
+                {Sip{\H{o}}cz}, B.~M. and {G{\"u}nther}, H.~M. and {Lim}, P.~L. and
+                {Crawford}, S.~M. and {Conseil}, S. and {Shupe}, D.~L. and
+                {Craig}, M.~W. and {Dencheva}, N. and {Ginsburg}, A. and {Vand
+                erPlas}, J.~T. and {Bradley}, L.~D. and {P{\'e}rez-Su{\'a}rez}, D. and
+                {de Val-Borro}, M. and {Aldcroft}, T.~L. and {Cruz}, K.~L. and
+                {Robitaille}, T.~P. and {Tollerud}, E.~J. and {Ardelean}, C. and
+                {Babej}, T. and {Bach}, Y.~P. and {Bachetti}, M. and {Bakanov}, A.~V. and
+                {Bamford}, S.~P. and {Barentsen}, G. and {Barmby}, P. and
+                {Baumbach}, A. and {Berry}, K.~L. and {Biscani}, F. and {Boquien}, M. and
+                {Bostroem}, K.~A. and {Bouma}, L.~G. and {Brammer}, G.~B. and
+                {Bray}, E.~M. and {Breytenbach}, H. and {Buddelmeijer}, H. and
+                {Burke}, D.~J. and {Calderone}, G. and {Cano Rodr{\'\i}guez}, J.~L. and
+                {Cara}, M. and {Cardoso}, J.~V.~M. and {Cheedella}, S. and {Copin}, Y. and
+                {Corrales}, L. and {Crichton}, D. and {D'Avella}, D. and {Deil}, C. and
+                {Depagne}, {\'E}. and {Dietrich}, J.~P. and {Donath}, A. and
+                {Droettboom}, M. and {Earl}, N. and {Erben}, T. and {Fabbro}, S. and
+                {Ferreira}, L.~A. and {Finethy}, T. and {Fox}, R.~T. and
+                {Garrison}, L.~H. and {Gibbons}, S.~L.~J. and {Goldstein}, D.~A. and
+                {Gommers}, R. and {Greco}, J.~P. and {Greenfield}, P. and
+                {Groener}, A.~M. and {Grollier}, F. and {Hagen}, A. and {Hirst}, P. and
+                {Homeier}, D. and {Horton}, A.~J. and {Hosseinzadeh}, G. and {Hu}, L. and
+                {Hunkeler}, J.~S. and {Ivezi{\'c}}, {\v{Z}}. and {Jain}, A. and
+                {Jenness}, T. and {Kanarek}, G. and {Kendrew}, S. and {Kern}, N.~S. and
+                {Kerzendorf}, W.~E. and {Khvalko}, A. and {King}, J. and {Kirkby}, D. and
+                {Kulkarni}, A.~M. and {Kumar}, A. and {Lee}, A. and {Lenz}, D. and
+                {Littlefair}, S.~P. and {Ma}, Z. and {Macleod}, D.~M. and
+                {Mastropietro}, M. and {McCully}, C. and {Montagnac}, S. and
+                {Morris}, B.~M. and {Mueller}, M. and {Mumford}, S.~J. and {Muna}, D. and
+                {Murphy}, N.~A. and {Nelson}, S. and {Nguyen}, G.~H. and
+                {Ninan}, J.~P. and {N{\"o}the}, M. and {Ogaz}, S. and {Oh}, S. and
+                {Parejko}, J.~K. and {Parley}, N. and {Pascual}, S. and {Patil}, R. and
+                {Patil}, A.~A. and {Plunkett}, A.~L. and {Prochaska}, J.~X. and
+                {Rastogi}, T. and {Reddy Janga}, V. and {Sabater}, J. and
+                {Sakurikar}, P. and {Seifert}, M. and {Sherbert}, L.~E. and
+                {Sherwood-Taylor}, H. and {Shih}, A.~Y. and {Sick}, J. and
+                {Silbiger}, M.~T. and {Singanamalla}, S. and {Singer}, L.~P. and
+                {Sladen}, P.~H. and {Sooley}, K.~A. and {Sornarajah}, S. and
+                {Streicher}, O. and {Teuben}, P. and {Thomas}, S.~W. and
+                {Tremblay}, G.~R. and {Turner}, J.~E.~H. and {Terr{\'o}n}, V. and
+                {van Kerkwijk}, M.~H. and {de la Vega}, A. and {Watkins}, L.~L. and
+                {Weaver}, B.~A. and {Whitmore}, J.~B. and {Woillez}, J. and
+                {Zabalza}, V. and {Astropy Contributors}},
+                title = "{The Astropy Project: Building an Open-science Project and Status of the v2.0 Core Package}",
+            journal = {\aj},
+            keywords = {methods: data analysis, methods: miscellaneous, methods: statistical, reference systems, Astrophysics - Instrumentation and Methods for Astrophysics},
+                year = 2018,
+                month = sep,
+            volume = {156},
+            number = {3},
+                eid = {123},
+                pages = {123},
+                doi = {10.3847/1538-3881/aabc4f},
+        archivePrefix = {arXiv},
+            eprint = {1801.02634},
+        primaryClass = {astro-ph.IM},
+            adsurl = {https://ui.adsabs.harvard.edu/abs/2018AJ....156..123A},
+            adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+        }"""]
     ),
 }
