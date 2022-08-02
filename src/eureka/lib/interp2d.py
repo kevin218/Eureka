f92037f7e0ec2c5cf49673d1b0d95db9ca740bee
sebsatian-zieba <zieba@mpia-hd.mpg.de>
2022-08-02 17:19:43 +0200

    fixed flake8 issues in interp2d.py

--- src/eureka/lib/interp2d.py
+++ src/eureka/lib/interp2d.py
@@ -12,21 +12,22 @@
 
 import numpy as np
 
+
 def interp2d(image, expand=5, y=None, x=None, yi=None, xi=None):
-  sz = np.shape(image)
-  imagen = np.zeros( expand * (np.array(sz) - 1) + 1 )
-  szi = np.shape(imagen)
+    sz = np.shape(image)
+    imagen = np.zeros(expand * (np.array(sz) - 1) + 1)
+    szi = np.shape(imagen)
 
-  if y is None:
-    y  = np.arange(sz[0])
-    x  = np.arange(sz[1])
-    yi = np.linspace(0, sz[0]-1, szi[0])
-    xi = np.linspace(0, sz[1]-1, szi[1])
+    if y is None:
+        y = np.arange(sz[0])
+        x = np.arange(sz[1])
+        yi = np.linspace(0, sz[0]-1, szi[0])
+        xi = np.linspace(0, sz[1]-1, szi[1])
 
-  for k in np.arange(sz[0]):
-    imagen[k] = np.interp(xi, x, image[k])
+    for k in np.arange(sz[0]):
+        imagen[k] = np.interp(xi, x, image[k])
 
-  for k in np.arange(szi[1]):
-    imagen[:, k] = np.interp(yi, y, imagen[0:sz[0], k])
+    for k in np.arange(szi[1]):
+        imagen[:, k] = np.interp(yi, y, imagen[0:sz[0], k])
 
-  return imagen
+    return imagen
