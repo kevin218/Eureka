b5a6aed70e623b46103c69184485aa447d9ebaef
sebsatian-zieba <zieba@mpia-hd.mpg.de>
2022-08-15 14:11:06 +0200

    add docstring to interp2d

--- src/eureka/lib/interp2d.py
+++ src/eureka/lib/interp2d.py
@@ -2,6 +2,30 @@ import numpy as np
 
 
 def interp2d(image, expand=5, y=None, x=None, yi=None, xi=None):
+    """
+    This function oversamples a 2D frame (image) which can be used if the user decides
+    that the resolution of the image is not enough and they want to split a pixel
+    into more subpixels.
+
+    Parameters
+    ----------
+    image : 2D numpy array
+        Contains the 2D frame which will be oversampled in pixels
+    expand : int
+        The factor by which a pixel should be oversampled. If set to 5, a pixel will turn into 25 subpixels.
+    y : 1D numpy array
+        np.arange(ny), with ny being the number of pixels in the y direction
+    x : 1D numpy array
+        np.arange(nx), with ny being the number of pixels in the x direction
+    yi : 1D numpy array
+        np.linspace(0, ny - 1, isz[0]),
+        with isz = np.array(sz, dtype=int) + (np.array(sz, dtype=int) - 1) * (expand - 1)
+        and sz = np.shape(image)
+    xi : 1D numpy array
+        np.linspace(0, ny - 1, isz[0]),
+        with isz = np.array(sz, dtype=int) + (np.array(sz, dtype=int) - 1) * (expand - 1)
+        and sz = np.shape(image)
+    """
     sz = np.shape(image)
     imagen = np.zeros(expand * (np.array(sz) - 1) + 1)
     szi = np.shape(imagen)
