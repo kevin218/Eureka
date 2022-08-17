e1beb2cb8d73ba477b4d8606c7cc7bc0bfbeaf01
sebsatian-zieba <zieba@mpia-hd.mpg.de>
2022-08-17 14:28:04 +0200

    fixing flake8 in plots_s3.py

--- src/eureka/S3_data_reduction/plots_s3.py
+++ src/eureka/S3_data_reduction/plots_s3.py
@@ -269,7 +269,8 @@ def source_position(meta, x_dim, pos_max, m, n,
 
 
 def profile(meta, profile, submask, n, m):
-    '''Plot weighting profile from optimal spectral extraction routine. (Figs 3303)
+    '''Plot weighting profile from optimal spectral extraction routine.
+    (Figs 3303)
 
     Parameters
     ----------
@@ -573,7 +574,8 @@ def median_frame(data, meta):
 # Photometry
 def phot_lc(data, meta):
     """
-    Plots the flux as determined by the photometry routine as a function of time. (Fig 3107)
+    Plots the flux as determined by the photometry routine as a function of
+    time. (Fig 3107)
 
     Parameters
     ----------
@@ -604,8 +606,8 @@ def phot_lc(data, meta):
 
 def phot_bg(data, meta):
     """
-    Plots the background flux as determined by the photometry routine as a function of time.
-    (Fig 3305)
+    Plots the background flux as determined by the photometry routine as a
+    function of time. (Fig 3305)
 
     Parameters
     ----------
@@ -625,7 +627,8 @@ def phot_bg(data, meta):
         plt.figure(3305)
         plt.clf()
         plt.suptitle('Photometric background light curve')
-        plt.errorbar(data.time, data['skylev'], yerr=data['skyerr'], c='k', fmt='.')
+        plt.errorbar(data.time, data['skylev'], yerr=data['skyerr'],
+                     c='k', fmt='.')
         plt.ylabel('Flux')
         plt.xlabel('Time')
         plt.tight_layout()
@@ -637,8 +640,8 @@ def phot_bg(data, meta):
 
 def phot_centroid(data, meta):
     """
-    Plots the (x, y) centroids and (sx, sy) the Gaussian 1-sigma half-widths as a function of time.
-    (Fig 3108)
+    Plots the (x, y) centroids and (sx, sy) the Gaussian 1-sigma half-widths
+    as a function of time. (Fig 3108)
 
     Parameters
     ----------
@@ -692,8 +695,8 @@ def phot_centroid(data, meta):
 
 def phot_npix(data, meta):
     """
-    Plots the number of pixels within the target aperture and within the background annulus
-    as a function of time. (Fig 3502)
+    Plots the number of pixels within the target aperture and within the
+    background annulus as a function of time. (Fig 3502)
 
     Parameters
     ----------
@@ -733,7 +736,8 @@ def phot_centroid_fgc(img, x, y, sx, sy, i, m, meta):
     Parameters
     ----------
     img : 2D numpy array
-        Cutout of the center of the target which is used to determine the centroid position.
+        Cutout of the center of the target which is used to determine the
+        centroid position.
     x : float
         Centroid position in x direction.
     y : float
@@ -767,7 +771,8 @@ def phot_centroid_fgc(img, x, y, sx, sy, i, m, meta):
     ax[1, 0].plot(range(len(np.sum(img, axis=0))), np.sum(img, axis=0))
     x_plot = np.linspace(0, len(np.sum(img, axis=0)))
     norm_distr_x = stats.norm.pdf(x_plot, x, sx)
-    norm_distr_x_scaled = norm_distr_x/np.max(norm_distr_x)*np.max(np.sum(img, axis=0))
+    norm_distr_x_scaled = \
+        norm_distr_x/np.max(norm_distr_x)*np.max(np.sum(img, axis=0))
     ax[1, 0].plot(x_plot, norm_distr_x_scaled)
     ax[1, 0].set_xlabel('x position')
     ax[1, 0].set_ylabel('Flux (electrons)')
@@ -775,7 +780,8 @@ def phot_centroid_fgc(img, x, y, sx, sy, i, m, meta):
     ax[0, 1].plot(np.sum(img, axis=1), range(len(np.sum(img, axis=1))))
     y_plot = np.linspace(0, len(np.sum(img, axis=1)))
     norm_distr_y = stats.norm.pdf(y_plot, y, sy)
-    norm_distr_y_scaled = norm_distr_y/np.max(norm_distr_y)*np.max(np.sum(img, axis=1))
+    norm_distr_y_scaled = \
+        norm_distr_y/np.max(norm_distr_y)*np.max(np.sum(img, axis=1))
     ax[0, 1].plot(norm_distr_y_scaled, y_plot)
     ax[0, 1].set_ylabel('y position')
     ax[0, 1].set_xlabel('Flux (electrons)')
@@ -783,8 +789,8 @@ def phot_centroid_fgc(img, x, y, sx, sy, i, m, meta):
     file_number = str(m).zfill(int(np.floor(np.log10(meta.num_data_files))+1))
     int_number = str(i).zfill(int(np.floor(np.log10(meta.n_int))+1))
     plt.tight_layout()
-    fname = (f'figs{os.sep}fig3503_file{file_number}_int{int_number}_Centroid_Fit'
-             + figure_filetype)
+    fname = (f'figs{os.sep}fig3503_file{file_number}_int{int_number}'
+             f'_Centroid_Fit' + figure_filetype)
     plt.savefig(meta.outputdir + fname, dpi=250)
     if not meta.hide_plots:
         plt.pause(0.2)
@@ -794,7 +800,8 @@ def add_colorbar(im, aspect=20, pad_fraction=0.5, **kwargs):
     """
     Add a vertical color bar to an image plot.
     Taken from:
-    https://stackoverflow.com/questions/18195758/set-matplotlib-colorbar-size-to-match-graph
+    https://stackoverflow.com/
+    questions/18195758/set-matplotlib-colorbar-size-to-match-graph
     """
     divider = axes_grid1.make_axes_locatable(im.axes)
     width = axes_grid1.axes_size.AxesY(im.axes, aspect=1./aspect)
@@ -808,8 +815,9 @@ def add_colorbar(im, aspect=20, pad_fraction=0.5, **kwargs):
 def phot_2d_frame(data, meta, m, i):
     """
     Plots the 2D frame together with the centroid position, the target aperture
-    and the background annulus. (Fig 3306) If meta.isplots_S3 >= 5, this function will additionally
-    create another figure - Fig 3504 - but it only includes the target area. (Fig 3306 and 3504)
+    and the background annulus. (Fig 3306) If meta.isplots_S3 >= 5, this
+    function will additionally create another figure - Fig 3504 - but it
+    only includes the target area. (Fig 3306 and 3504)
 
     Parameters
     ----------
@@ -833,14 +841,18 @@ def phot_2d_frame(data, meta, m, i):
     plt.clf()
     plt.suptitle('2D frame with centroid and apertures')
 
-    flux, centroid_x, centroid_y = data.flux[i], data.centroid_x[i], data.centroid_y[i]
+    flux, centroid_x, centroid_y = \
+        data.flux[i], data.centroid_x[i], data.centroid_y[i]
 
-    xmin, xmax = data.flux.x.min().values-meta.xwindow[0], data.flux.x.max().values-meta.xwindow[0]
-    ymin, ymax = data.flux.y.min().values-meta.ywindow[0], data.flux.y.max().values-meta.ywindow[0]
+    xmin = data.flux.x.min().values-meta.xwindow[0]
+    xmax = data.flux.x.max().values-meta.xwindow[0]
+    ymin = data.flux.y.min().values-meta.ywindow[0]
+    ymax = data.flux.y.max().values-meta.ywindow[0]
 
     im = plt.imshow(flux, vmin=0, vmax=5e3, origin='lower', aspect='equal',
                     extent=[xmin, xmax, ymin, ymax])
-    plt.scatter(centroid_x, centroid_y, marker='x', s=25, c='r', label='centroid')
+    plt.scatter(centroid_x, centroid_y, marker='x', s=25, c='r',
+                label='centroid')
     plt.title('Full 2D frame')
     plt.ylabel('y pixels')
     plt.xlabel('x pixels')
@@ -878,15 +890,18 @@ def phot_2d_frame(data, meta, m, i):
 
         im = plt.imshow(flux, vmin=0, vmax=5e3, origin='lower', aspect='equal',
                         extent=[xmin, xmax, ymin, ymax])
-        plt.scatter(centroid_x, centroid_y, marker='x', s=25, c='r', label='centroid')
+        plt.scatter(centroid_x, centroid_y, marker='x', s=25, c='r',
+                    label='centroid')
         plt.title('Zoom into 2D frame')
         plt.ylabel('y pixels')
         plt.xlabel('x pixels')
 
         circle1 = plt.Circle((centroid_x, centroid_y), meta.photap, color='r',
-                             fill=False, lw=3, alpha=0.7, label='target aperture')
+                             fill=False, lw=3, alpha=0.7,
+                             label='target aperture')
         circle2 = plt.Circle((centroid_x, centroid_y), meta.skyin, color='w',
-                             fill=False, lw=4, alpha=0.8, label='sky aperture')
+                             fill=False, lw=4, alpha=0.8,
+                             label='sky aperture')
         circle3 = plt.Circle((centroid_x, centroid_y), meta.skyout, color='w',
                              fill=False, lw=4, alpha=0.8)
         plt.gca().add_patch(circle1)
@@ -906,8 +921,8 @@ def phot_2d_frame(data, meta, m, i):
 
         plt.legend()
 
-        fname = (f'figs{os.sep}fig3504_file{file_number}_int{int_number}_2D_Frame_Zoom'
-                 + figure_filetype)
+        fname = (f'figs{os.sep}fig3504_file{file_number}_int{int_number}'
+                 f'_2D_Frame_Zoom' + figure_filetype)
         plt.savefig(meta.outputdir + fname, dpi=250)
         if not meta.hide_plots:
             plt.pause(0.2)
@@ -915,10 +930,11 @@ def phot_2d_frame(data, meta, m, i):
 
 def phot_2d_frame_oneoverf(data, meta, m, i, flux_w_oneoverf):
     """
-    Plots the 2D frame with a low vmax so that the background is well visible. The top panel
-    is before the 1/f correction, the lower panel shows the 2D frame after the 1/f correction.
-    The typical "stripy" structure for each row should have been mitigated after the 1/f correction
-    in Stage 3. (Fig 3307)
+    Plots the 2D frame with a low vmax so that the background is well visible.
+    The top panel is before the 1/f correction, the lower panel shows the 2D
+    frame after the 1/f correction. The typical "stripy" structure for each
+    row should have been mitigated after the 1/f correction in Stage 3.
+    (Fig 3307)
 
     Parameters
     ----------
@@ -944,12 +960,14 @@ def phot_2d_frame_oneoverf(data, meta, m, i, flux_w_oneoverf):
     plt.clf()
     fig, ax = plt.subplots(2, 1, figsize=(8.2, 4.2))
 
-    ax[0].imshow(flux_w_oneoverf, origin='lower', norm=LogNorm(vmin=0.1, vmax=40), cmap='viridis')
+    ax[0].imshow(flux_w_oneoverf, origin='lower',
+                 norm=LogNorm(vmin=0.1, vmax=40), cmap='viridis')
     ax[0].set_title('Before 1/f correction')
     ax[0].set_ylabel('y pixels')
 
     flux = data.flux.values[i]
-    im1 = ax[1].imshow(flux, origin='lower', norm=LogNorm(vmin=0.1, vmax=40), cmap='viridis')
+    im1 = ax[1].imshow(flux, origin='lower',
+                       norm=LogNorm(vmin=0.1, vmax=40), cmap='viridis')
     ax[1].set_title('After 1/f correction')
     ax[1].set_xlabel('x pixels')
     ax[1].set_ylabel('y pixels')
@@ -962,8 +980,8 @@ def phot_2d_frame_oneoverf(data, meta, m, i, flux_w_oneoverf):
     int_number = str(i).zfill(int(np.floor(np.log10(meta.n_int))+1))
     fig.suptitle((f'Segment {file_number}, Integration {int_number}'), y=0.99)
 
-    fname = (f'figs{os.sep}fig3307_file{file_number}_int{int_number}_2D_Frame_OneOverF' +
-             figure_filetype)
+    fname = (f'figs{os.sep}fig3307_file{file_number}_int{int_number}'
+             f'_2D_Frame_OneOverF' + figure_filetype)
     plt.savefig(meta.outputdir + fname, dpi=250)
     if not meta.hide_plots:
         plt.pause(0.2)
@@ -971,9 +989,9 @@ def phot_2d_frame_oneoverf(data, meta, m, i, flux_w_oneoverf):
 
 def phot_2d_frame_diff(data, meta):
     """
-    Plots the difference between to consecutive 2D frames. This might be helpful in order
-    to investigate flux changes due to mirror tilts which have been observed during commissioning.
-    (Fig 3505)
+    Plots the difference between to consecutive 2D frames. This might be
+    helpful in order to investigate flux changes due to mirror tilts
+    which have been observed during commissioning. (Fig 3505)
 
     Parameters
     ----------
@@ -1004,7 +1022,8 @@ def phot_2d_frame_diff(data, meta):
         plt.tight_layout()
         int_number = str(i).zfill(int(np.floor(np.log10(meta.n_int)) + 1))
         plt.suptitle((f'Integration {int_number}'), y=0.99)
-        fname = (f'figs{os.sep}fig3505_int{int_number}_2D_Frame_Diff' + figure_filetype)
+        fname = (f'figs{os.sep}fig3505_int{int_number}_2D_Frame_Diff'
+                 + figure_filetype)
         plt.savefig(meta.outputdir + fname, dpi=250)
         if not meta.hide_plots:
             plt.pause(0.2)
