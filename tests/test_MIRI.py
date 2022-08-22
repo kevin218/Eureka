b4f47cadf9114d704704b47dfccfb6b1ea602a20
Taylor Bell <bell@baeri.org>
2022-08-22 16:20:26 -0400

    Only WFC3 test uses pdf figures
    This will help avoid RAM overflow on GitHub test runs

--- tests/test_MIRI.py
+++ tests/test_MIRI.py
@@ -23,7 +23,7 @@ from eureka.S6_planet_spectra import s6_spectra as s6
 def test_MIRI(capsys):
     # Set up some parameters to make plots look nicer.
     # You can set usetex=True if you have LaTeX installed
-    eureka.lib.plots.set_rc(style='eureka', usetex=False, filetype='.pdf')
+    eureka.lib.plots.set_rc(style='eureka', usetex=False, filetype='.png')
 
     s2_installed = 'eureka.S2_calibrations.s2_calibrate' in sys.modules
     if not s2_installed:
