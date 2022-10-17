2e36c926b731465a6f9962c8adf51e731896808a
Taylor Bell <bell@baeri.org>
2022-10-17 10:58:19 -0700

    Trying to resolve bug with MIRI S6 test

--- src/eureka/S6_planet_spectra/s6_spectra.py
+++ src/eureka/S6_planet_spectra/s6_spectra.py
@@ -953,6 +953,9 @@ def roundToSigFigs(x, sigFigs=2):
     """
     if not np.isfinite(x) or not np.isfinite(np.log10(np.abs(x))):
         return np.nan, ""
+    elif not np.isfinite(sigFigs):
+        return 10, str(np.round(x, 10))
+
     nDec = -int(np.floor(np.log10(np.abs(x))))+sigFigs-1
     rounded = np.round(x, nDec)
     if nDec <= 0:
@@ -1087,12 +1090,16 @@ def transit_latex_table(meta, log):
             val = line[meta.y_param+'_value']*meta.y_scalar
             upper = line[meta.y_param+'_errorpos']
             lower = line[meta.y_param+'_errorneg']
-            if np.isnan(upper) and np.isnan(lower):
+            if not (np.isfinite(upper) or np.isfinite(lower)):
                 nDec = 10
             else:
                 nDec1, _ = roundToSigFigs(upper*meta.y_scalar)
                 nDec2, _ = roundToSigFigs(lower*meta.y_scalar)
-                nDec = int(np.nanmax([nDec1, nDec2]))
+                nDec = np.nanmax([nDec1, nDec2])
+                if not np.isfinite(nDec):
+                    nDec = 10
+                else:
+                    nDec = int(nDec)
             val = roundToDec(val, nDec)
             upper = roundToDec(upper, nDec)
             lower = roundToDec(lower, nDec)
