Fixing NIRCam wave_2d calculation for F322W2
Before there were issues with converting wave_1d to wave_2d. Using
np.repeat to take care of that

src/eureka/S3_data_reduction/nircam.py
@@ -75,7 +75,8 @@ def read(filename, data, meta, log):
                        + 0.001666535535484272*Xprime**2
                        - 0.002874123523765872*Xprime**3)
             # Convert 1D array to 2D
-            wave_2d *= np.ones((hdulist['WAVELENGTH', 1].data.shape[0], 1))
+            wave_2d = np.repeat(wave_2d[np.newaxis],
+                                hdulist['WAVELENGTH', 1].data.shape[0], axis=0)
     elif hdulist[0].header['CHANNEL'] == 'SHORT':
         # Photometry will have "SHORT" as CHANNEL
         meta.photometry = True