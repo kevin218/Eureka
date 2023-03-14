f3d1c5af659ebdb4dfb4b2024f8cfe6ec4a34d87
Taylor Bell <bell@baeri.org>
2023-03-13 17:47:36 -0700

    Fixing orbital phase for e>0 PyMC3 sinusoid pc

--- src/eureka/S5_lightcurve_fitting/differentiable_models/SinusoidPhaseCurve.py
+++ src/eureka/S5_lightcurve_fitting/differentiable_models/SinusoidPhaseCurve.py
@@ -132,9 +132,14 @@ class SinusoidPhaseCurveModel(PyMC3Model):
             phaseVars = lib.ones(len(self.time))
             
             # Compute orbital phase
-            # FINDME: for now this assumes a circular orbit
-            t = self.time - model.t0 - model.per/2.
-            phi = 2.*np.pi/model.per*t
+            if model.ecc == 0.:
+                # the planet is on a circular orbit
+                t = self.time - model.t0 - model.per/2.
+                phi = 2.*np.pi/model.per*t
+            else:
+                # the planet is on an eccentric orbit
+                anom = true_anomaly(model, lib, self.time)
+                phi = anom + model.w*np.pi/180. + np.pi/2.
 
             for order in range(1, self.maxOrder+1):
                 if chan == 0:
@@ -160,3 +165,189 @@ class SinusoidPhaseCurveModel(PyMC3Model):
             lcfinal = lib.concatenate([lcfinal, lcpiece])
 
         return lcfinal
+
+
+def true_anomaly(model, lib, t, xtol=1e-10):
+    """Convert time to true anomaly, numerically.
+
+    Parameters
+    ----------
+    model : pymc3.Model
+        The PyMC3 model (which contains the orbital parameters).
+    lib : library
+        Either np (numpy) or tt (theano.tensor), depending on whether the
+        code is being run in numpy or theano mode.
+    t : ndarray
+        The time in days.
+    xtol : float; optional
+        tolarance on error in eccentric anomaly (calculated along the way).
+        Defaults to 1e-10.
+
+    Returns
+    -------
+    ndarray
+        The true anomaly in radians.
+
+    Notes
+    -----
+    History:
+
+    - March 2023 Taylor Bell
+        Based on Bell_EBM code, but modified to enable theano code.
+    """
+    return 2.*lib.arctan(lib.sqrt((1.+model.ecc)/(1.-model.ecc)) *
+                         lib.tan(eccentric_anomaly(model, lib, t,
+                                                   xtol=xtol)/2.))
+
+
+def eccentric_anomaly(model, lib, t, xtol=1e-10):
+    """Convert time to eccentric anomaly, numerically.
+
+    Parameters
+    ----------
+    model : pymc3.Model
+        The PyMC3 model (which contains the orbital parameters).
+    lib : library
+        Either np (numpy) or tt (theano.tensor), depending on whether the
+        code is being run in numpy or theano mode.
+    t : ndarray
+        The time in days.
+    xtol : float; optional
+        tolarance on error in eccentric anomaly. Defaults to 1e-10.
+
+    Returns
+    -------
+    ndarray
+        The eccentric anomaly in radians.
+
+    Notes
+    -----
+    History:
+
+    - March 2023 Taylor Bell
+        Based on Bell_EBM code, but modified to enable theano code.
+    """
+    ta_peri = np.pi/2.-model.w*np.pi/180.
+    ea_peri = 2.*lib.arctan(lib.sqrt((1.-model.ecc)/(1.+model.ecc)) *
+                            lib.tan(ta_peri/2.))
+    ma_peri = ea_peri - model.ecc*np.sin(ea_peri)
+    t_peri = (model.t0 - (ma_peri/(2.*np.pi)*model.per))
+
+    if ((lib == tt and tt.lt(t_peri, 0)) or 
+            (t_peri < 0)):
+        t_peri = t_peri + model.per
+
+    M = ((t-t_peri) * 2.*np.pi/model.per) % (2.*np.pi)
+
+    E = FSSI_Eccentric_Inverse(model, lib, M, xtol)
+
+    return E
+
+
+def FSSI_Eccentric_Inverse(model, lib, M, xtol=1e-10):
+    """Convert mean anomaly to eccentric anomaly using FSSI algorithm.
+    
+    Algorithm based on that from Tommasini+2018.
+
+    Parameters
+    ----------
+    model : pymc3.Model
+        The PyMC3 model (which contains the orbital parameters).
+    lib : library
+        Either np (numpy) or tt (theano.tensor), depending on whether the
+        code is being run in numpy or theano mode.
+    M : ndarray
+        The mean anomaly in radians.
+    xtol : float; optional
+        tolarance on error in eccentric anomaly. Defaults to 1e-10.
+
+    Returns
+    -------
+    ndarray
+        The eccentric anomaly in radians.
+
+    Notes
+    -----
+    History:
+
+    - March 2023 Taylor Bell
+        Based on Bell_EBM code, but modified to enable theano code.
+    """
+    xtol = np.max([1e-15, xtol])
+    nGrid = (xtol/100.)**(-1./4.)
+
+    xGrid = lib.arange(0, 2.*np.pi, 2*np.pi/int(nGrid))
+
+    def f(ea):
+        return ea - model.ecc*lib.sin(ea)
+
+    def fP(ea):
+        return 1. - model.ecc*lib.cos(ea)
+
+    return FSSI(lib, M, x=xGrid, f=f, fP=fP)
+
+
+def FSSI(lib, Y, x, f, fP):
+    """Fast Switch and Spline Inversion method from Tommasini+2018.
+
+    Parameters
+    ----------
+    lib : library
+        Either np (numpy) or tt (theano.tensor), depending on whether the
+        code is being run in numpy or theano mode.
+    Y : ndarray
+        The f(x) values to invert.
+    x : ndarray
+        x values spanning the domain (more values for higher precision).
+    f : callable
+        The function f.
+    fP : callable
+        The first derivative of the function f with respect to x.
+
+    Returns
+    -------
+    ndarray
+        The numerical approximation of f^-(y).
+
+    Notes
+    -----
+    History:
+
+    - March 2023 Taylor Bell
+        Based on Bell_EBM code, but modified to enable theano code.
+    """
+    y = f(x)
+    d = 1./fP(x)
+
+    x0 = x[:-1]
+    x1 = x[1:]
+    y0 = y[:-1]
+    y1 = y[1:]
+    d0 = d[:-1]
+    d1 = d[1:]
+
+    c0 = x0
+    c1 = d0
+
+    dx = x0 - x1
+    dy = y0 - y1
+    dy2 = dy*dy
+
+    c2 = ((2.*d0 + d1)*dy - 3.*dx)/dy2
+    c3 = ((d0 + d1)*dy - 2.*dx)/(dy2*dy)
+
+    if lib == np:
+        j = np.searchsorted(y1, Y)
+        # Protect against indexing beyond the size of the array
+        j[j >= y1.size] = y1.size-1
+    else:
+        j = tt.extra_ops.searchsorted(y1, Y)
+        # Protect against indexing beyond the size of the array
+        # Theano tensors don't allow item assignment
+        mask = tt.ge(j, y1.size)
+        j = j*(1-mask) + (y1.size-1)*mask
+
+    P1 = Y - y0[j]
+    P2 = P1*P1
+
+    return c0[j] + c1[j]*P1 + c2[j]*P2 + c3[j]*P2*P1
