a2750a5fe792a3a21073053ac55fb2d5e30d045b
Hannah Diamond-Lowe <h.diamondlowe@gmail.com>
2023-11-18 17:00:06 +0100

    reverting back plots_s3

--- src/eureka/S3_data_reduction/plots_s3.py
+++ src/eureka/S3_data_reduction/plots_s3.py
@@ -886,40 +886,30 @@ def phot_centroid_fgc(img, x, y, sx, sy, i, m, meta):
     # Title
     plt.suptitle('Centroid gaussian fit')
 
-    #print("TEST!! x, sx", x, sx)
-    #print("TEST!! y, sy", y, sy)
-
     # Image of source
-    vmin, vmax = np.percentile(img[~np.isnan(img)], 3), np.percentile(img[~np.isnan(img)], 97)
-    ax[1, 0].imshow(img, origin='lower', aspect='auto', vmin=vmin, vmax=vmax)
-
-    ax[1, 0].axhline(y, color='C3', alpha=0.7)
-    ax[1, 0].axvline(x, color='C3', alpha=0.7)
-
+    ax[1, 0].imshow(img, origin='lower', aspect='auto')
 
     # X gaussian plot
-    med_x = np.nanmedian(np.nansum(img, axis=0))
     norm_x_factor = np.nansum(np.nansum(img, axis=0))
     ax[0, 0].plot(range(len(np.nansum(img, axis=0))),
-                  (np.nansum(img, axis=0)-med_x)/norm_x_factor)
+                  (np.nansum(img, axis=0))/norm_x_factor)
     x_plot = np.linspace(0, len(np.nansum(img, axis=0)))
     norm_distr_x = stats.norm.pdf(x_plot, x, sx)
     norm_distr_x_scaled = \
-        norm_distr_x/np.nanmax(norm_distr_x)*np.nanmax(np.nansum(img, axis=0)-med_x)
+        norm_distr_x/np.nanmax(norm_distr_x)*np.nanmax(np.nansum(img, axis=0))
     ax[0, 0].plot(x_plot, norm_distr_x_scaled/norm_x_factor,
                   linestyle='dashed')
     ax[0, 0].set_xlabel('x position')
     ax[0, 0].set_ylabel('Normalized Flux')
 
     # Y gaussian plot
-    med_y = np.nanmedian(np.nansum(img, axis=1))
     norm_y_factor = np.nansum(np.nansum(img, axis=0))
-    ax[1, 1].plot((np.nansum(img, axis=1)-med_y)/norm_y_factor,
+    ax[1, 1].plot((np.nansum(img, axis=1))/norm_y_factor,
                   range(len(np.nansum(img, axis=1))))
     y_plot = np.linspace(0, len(np.nansum(img, axis=1)))
     norm_distr_y = stats.norm.pdf(y_plot, y, sy)
     norm_distr_y_scaled = \
-        norm_distr_y/np.nanmax(norm_distr_y)*np.nanmax(np.nansum(img, axis=1)-med_y)
+        norm_distr_y/np.nanmax(norm_distr_y)*np.nanmax(np.nansum(img, axis=1))
     ax[1, 1].plot(norm_distr_y_scaled/norm_y_factor, y_plot,
                   linestyle='dashed')
     ax[1, 1].set_ylabel('y position')
@@ -1031,7 +1021,7 @@ def phot_2d_frame(data, meta, m, i):
         plt.pause(0.2)
 
     if meta.isplots_S3 >= 5:
-        plt.figure(3504, figsize=(8, 8))
+        plt.figure(3504, figsize=(6, 5))
         plt.clf()
         plt.suptitle('2D frame with centroid and apertures (zoom-in version)')
 
