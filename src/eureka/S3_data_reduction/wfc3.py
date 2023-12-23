7641554c41764782ce12e4eb6a030dbb68b7f2be
Kevin Stevenson <kbstevenson@gmail.com>
2023-12-23 12:56:36 -0500

    Test new setup.cfg

--- src/eureka/S3_data_reduction/wfc3.py
+++ src/eureka/S3_data_reduction/wfc3.py
@@ -630,13 +630,11 @@ def difference_frames(data, meta, log):
             pass
 
         # Guess spectrum position only using subarray region
-        masked_data = diffflux[n, 
-                               meta.ywindow[0]:meta.ywindow[1],
-                               meta.xwindow[0]:meta.xwindow[1]] * \
-                      diffmask[n, 
-                               meta.ywindow[0]:meta.ywindow[1],
-                               meta.xwindow[0]:meta.xwindow[1]]
-        guess[n] = (np.median(np.where(masked_data > np.mean(masked_data))[0]) \
+        masked_data = diffflux[n, meta.ywindow[0]:meta.ywindow[1],
+                                  meta.xwindow[0]:meta.xwindow[1]] * \
+                      diffmask[n, meta.ywindow[0]:meta.ywindow[1],
+                                  meta.xwindow[0]:meta.xwindow[1]]
+        guess[n] = (np.median(np.where(masked_data > np.mean(masked_data))[0]) 
                     + meta.ywindow[0]).astype(int)
     # Guess may be skewed if first read is zeros
     if guess[0] < 0 or guess[0] > meta.ny:
