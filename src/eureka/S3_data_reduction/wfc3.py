0a79135fd93b2ec4f3fc772e6906000d2951a883
Yoni Brande <jbrande@ku.edu>
2024-05-13 12:50:52 -0500

    Addressing #649

--- src/eureka/S3_data_reduction/wfc3.py
+++ src/eureka/S3_data_reduction/wfc3.py
@@ -159,7 +159,7 @@ def conclusion_step(data, meta, log):
     log : logedit.Logedit
         The updated log.
     """
-    meta.centroids = np.array(meta.centroids)
+    meta.centroids = np.array(meta.centroids, dtype=object)
     meta.guess = np.array(meta.guess)
     meta.subdata_ref = np.array(meta.subdata_ref)
     meta.subdiffmask_ref = np.array(meta.subdiffmask_ref)
