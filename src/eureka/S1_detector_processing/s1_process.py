7686b48105833abe8c9a46a4f8bab1fb93b1bd3e
Taylor Bell <bell@baeri.org>
2024-08-19 15:45:30 -0700

    Adding S1 custom_mask and mask_file to s1_meta.py

--- src/eureka/S1_detector_processing/s1_process.py
+++ src/eureka/S1_detector_processing/s1_process.py
@@ -183,7 +183,7 @@ class EurekaS1Pipeline(Detector1Pipeline):
         # Instrument Non-Specific Steps
         self.group_scale.skip = meta.skip_group_scale
         self.dq_init.skip = meta.skip_dq_init
-        if hasattr(meta, 'custom_mask') and meta.custom_mask:
+        if meta.custom_mask:
             self.dq_init.override_mask = meta.mask_file
         self.saturation.skip = meta.skip_saturation
         self.ipc.skip = meta.skip_ipc
