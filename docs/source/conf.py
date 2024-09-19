d0f913eea72af3b73da264ad43a62a8d41128ef6
Taylor James Bell <bell@baeri.org>
2024-09-18 21:04:06 -0700

    Adding a dark mode to the readthedocs page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,9 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
+html_css_files = ["css/rtd_dark.css"]
+html_logo = "../media/Eureka_logo.png"
 
 # Ignoring duplicated section warnings in api file
 suppress_warnings = ['autosectionlabel.*']
