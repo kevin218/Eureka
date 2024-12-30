927433525a47823a294d2061fb79c6b23ba65883
Kevin Stevenson <kbstevenson@gmail.com>
2024-12-30 18:13:59 -0500

    Adds tests for S4cal

--- tests/test_S4cal.py
+++ tests/test_S4cal.py
@@ -13,11 +13,10 @@ from eureka.lib.util import COMMON_IMPORTS, pathdirectory
 import eureka.lib.plots
 from eureka.S2_calibrations import s2_calibrate as s2
 from eureka.S3_data_reduction import s3_reduce as s3
-from eureka.S4_generate_lightcurves import s4_genLC as s4
-from eureka.S5_lightcurve_fitting import s5_fit as s5
+from eureka.S4cal_StellarSpectra import s4cal_StellarSpec as s4cal
 
 
-def test_NIRSpec(capsys):
+def test_S4cal(capsys):
     # Set up some parameters to make plots look nicer.
     # You can set usetex=True if you have LaTeX installed
     eureka.lib.plots.set_rc(style='eureka', usetex=False, filetype='.png')
@@ -28,7 +27,7 @@ def test_NIRSpec(capsys):
         print("\n\nIMPORTANT: Make sure that any changes to the ecf files "
                 "are\nincluded in demo ecf files and documentation "
                 "(docs/source/ecf.rst).")
-        print("\nNIRSpec S2-5 test: ", end='', flush=True)
+        print("\nCalibrated Stellar Spectra S2-4 test: ", end='', flush=True)
 
     # explicitly define meta variables to be able to run
     # pathdirectory fn locally
@@ -36,24 +35,17 @@ def test_NIRSpec(capsys):
     meta.eventlabel = 'NIRSpec'
     meta.datetime = time_pkg.strftime('%Y-%m-%d')
     meta.topdir = f'..{os.sep}tests'
-    ecf_path = f'.{os.sep}NIRSpec_ecfs{os.sep}'
+    ecf_path = f'.{os.sep}S4cal_ecfs{os.sep}'
 
     reload(s2)
-    reload(s3)
-    reload(s4)
-    reload(s5)
     s2_meta = s2.calibrateJWST(meta.eventlabel, ecf_path=ecf_path)
-
-    s2_cites = np.union1d(COMMON_IMPORTS[1], ["nirspec"])
-    assert np.array_equal(s2_meta.citations, s2_cites)
-
-    s3_cites = np.union1d(s2_cites, COMMON_IMPORTS[2])
-
+    reload(s3)
     s3_spec, s3_meta = s3.reduce(meta.eventlabel, ecf_path=ecf_path,
                                  s2_meta=s2_meta)
-    s4_spec, s4_lc, s4_meta = s4.genlc(meta.eventlabel, ecf_path=ecf_path,
-                                       s3_meta=s3_meta)
-    s5_meta = s5.fitlc(meta.eventlabel, ecf_path=ecf_path, s4_meta=s4_meta)
+    reload(s4cal)
+    s4_meta, s4_spec, s4_ds = s4cal.medianCalSpec(meta.eventlabel,
+                                                  ecf_path=ecf_path,
+                                                  s3_meta=s3_meta)
 
     # run assertions for S2
     meta.outputdir_raw = (f'data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}'
@@ -66,37 +58,20 @@ def test_NIRSpec(capsys):
     # run assertions for S3
     meta.outputdir_raw = (f'data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}'
                           f'Stage3{os.sep}')
-    name = pathdirectory(meta, 'S3', 1, ap=5, bg=10,
+    name = pathdirectory(meta, 'S3', 1, ap=5, bg=6,
                          old_datetime=s3_meta.datetime)
     assert os.path.exists(name)
     assert os.path.exists(name+os.sep+'figs')
 
-    assert np.array_equal(s3_meta.citations, s3_cites)
-
-    # run assertions for S4
+    # run assertions for S4cal
     meta.outputdir_raw = (f'data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}'
-                          f'Stage4{os.sep}')
-    name = pathdirectory(meta, 'S4', 1, ap=5, bg=10,
+                          f'Stage4cal{os.sep}')
+    name = pathdirectory(meta, 'S4cal', 1,
                          old_datetime=s4_meta.datetime)
     assert os.path.exists(name)
     assert os.path.exists(name+os.sep+'figs')
 
-    s4_cites = np.union1d(s3_cites, COMMON_IMPORTS[3])
-    assert np.array_equal(s4_meta.citations, s4_cites)
-
-    # run assertions for S5
-    meta.outputdir_raw = (f'data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}'
-                          f'Stage5{os.sep}')
-    name = pathdirectory(meta, 'S5', 1, ap=5, bg=10,
-                         old_datetime=s5_meta.datetime)
-    assert os.path.exists(name)
-    assert os.path.exists(name+os.sep+'figs')
-
-    s5_cites = np.union1d(s4_cites, COMMON_IMPORTS[4] + ["batman"])
-    assert np.array_equal(s5_meta.citations, s5_cites)
-
     # remove temporary files
     os.system(f"rm -r data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}Stage2")
     os.system(f"rm -r data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}Stage3")
-    os.system(f"rm -r data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}Stage4")
-    os.system(f"rm -r data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}Stage5")
+    os.system(f"rm -r data{os.sep}JWST-Sim{os.sep}NIRSpec{os.sep}Stage4cal")
