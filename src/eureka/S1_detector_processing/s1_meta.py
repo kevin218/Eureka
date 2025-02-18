b157a903d647a32a59a1e0001b66589c10140a2c
Taylor J Bell <tbell@stsci.edu>
2025-02-18 14:07:35 -0500

    Adding safer meta.src_ypos default for S1

--- src/eureka/S1_detector_processing/s1_meta.py
+++ src/eureka/S1_detector_processing/s1_meta.py
@@ -187,7 +187,7 @@ class S1MetaClass(MetaClass):
             # Row-by-row BG subtraction (only useful for NIRCam)
             self.bg_row_by_row = getattr(self, 'bg_row_by_row', False)
             self.orders = getattr(self, 'orders', None)
-            self.src_ypos = getattr(self, 'src_ypos', 15)
+            self.src_ypos = getattr(self, 'src_ypos', None)
         # bg_x1 and bg_x2 also need to be defined if meta.masktrace is True
         # Left edge of exclusion region for row-by-row BG subtraction
         self.bg_x1 = getattr(self, 'bg_x1', None)
