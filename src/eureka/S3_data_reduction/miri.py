f63bcdbeee064fb9bee3ac28abe8820c10d7aa48
Taylor J Bell <tbell@stsci.edu>
2025-02-20 12:46:20 -0500

    Adding miri.flag_bg_phot

--- src/eureka/S3_data_reduction/miri.py
+++ src/eureka/S3_data_reduction/miri.py
@@ -9,7 +9,7 @@ except ImportError:
           'wavelength solution will not be able to be calculated in Stage 3.')
 from ..lib.util import read_time, supersample
 
-__all__ = ['read', 'straighten_trace', 'flag_ff', 'flag_bg',
+__all__ = ['read', 'straighten_trace', 'flag_ff', 'flag_bg', 'flag_bg_phot',
            'fit_bg', 'cut_aperture', 'standard_spectrum', 'clean_median_flux',
            'calibrated_spectra', 'residualBackground', 'lc_nodriftcorr']
 
@@ -279,6 +279,26 @@ def flag_bg(data, meta, log):
         The updated Dataset object with outlier background pixels flagged.
     '''
     return nircam.flag_bg(data, meta, log)
+
+
+def flag_bg_phot(data, meta, log):
+    '''Outlier rejection of sky background along time axis for photometry.
+
+    Parameters
+    ----------
+    data : Xarray Dataset
+        The Dataset object.
+    meta : eureka.lib.readECF.MetaClass
+        The metadata object.
+    log : logedit.Logedit
+        The current log.
+
+    Returns
+    -------
+    data : Xarray Dataset
+        The updated Dataset object with outlier background pixels flagged.
+    '''
+    return nircam.flag_bg_phot(data, meta, log)
 
 
 def flag_ff(data, meta, log):
