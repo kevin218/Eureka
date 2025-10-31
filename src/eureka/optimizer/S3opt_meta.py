131af91d8facbe554e6e04fc0fc03ad9bcd23d98
Kevin Stevenson <kbstevenson@gmail.com>
2025-10-31 13:55:09 -0400

    Apply instrument specific defaults

--- src/eureka/optimizer/S3opt_meta.py
+++ src/eureka/optimizer/S3opt_meta.py
@@ -2,6 +2,7 @@ import numpy as np
 from jwst.datamodels import JwstDataModel
 
 from ..lib.readECF import MetaClass
+from ..lib import util
 
 
 class S3optMetaClass(MetaClass):
@@ -38,8 +39,33 @@ class S3optMetaClass(MetaClass):
         # Set a default data file suffix
         self.suffix = getattr(self, 'suffix', 'calints')
 
+        # Create list of file segments
+        self = util.readfiles(self)
+
+        # First apply any instrument-specific defaults
+        if self.photometry:
+            if self.inst == 'miri':
+                self.set_MIRI_Photometry_defaults()
+            elif self.inst == 'nircam':
+                self.set_NIRCam_Photometry_defaults()
+        else:
+            if self.inst == 'miri':
+                self.set_MIRI_defaults()
+            elif self.inst == 'nircam':
+                self.set_NIRCam_defaults()
+            elif self.inst == 'nirspec':
+                self.set_NIRSpec_defaults()
+            elif self.inst == 'niriss':
+                self.set_NIRISS_defaults()
+            elif self.inst == 'wfc3':
+                self.set_WFC3_defaults()
+        # Then apply instrument-agnostic defaults
+        self.set_defaults()
+
+
     def set_defaults(self):
-        '''Set Optimizer specific defaults for generic instruments.
+        '''
+        Set Optimizer specific defaults for generic instruments.
         '''
         defaults = {
             "inputdir": 'Stage2',
@@ -51,20 +77,25 @@ class S3optMetaClass(MetaClass):
 
 
     def set_spectral_defaults(self):
-        '''Set Optimizer specific defaults for generic spectroscopic data.
+        '''
+        Set Optimizer specific defaults for generic spectroscopic data.
         '''
         # Spectral extraction parameters
         defaults = {
+            # Stage 3
             "bounds_dqmask": [True, False],  # not a range
-            "bounds_bg_thresh": range(3, 8),  # 3 → 7
+            "bounds_bg_thresh": range(3, 5.5, 0.5),  # 3 → 5, step 0.5
             "bounds_bg_method": ["std", "mean", "median"],
+            "bounds_bg_deg": range(0, 2),  # 0 → 1
             "bounds_p3thresh": range(3, 8),  # 3 → 7
-            "bounds_median_thresh": range(3, 8),  # 3 → 7
+            "bounds_median_thresh": range(3, 10),  # 3 → 9
             "bounds_window_len": range(1, 22, 2),  # 1 → 21, odd only
-            "bounds_p7thresh": range(5, 96, 5),  # 5 → 95 step 5
-            # "bounds_expand": range(1, 6),  # uncomment if needed, 1 → 5
+            "bounds_p7thresh": range(5, 61, 5),  # 5 → 60, step 5
+            # Stage 4
             "bounds_sigma": range(3, 8),  # 3 → 7
-            "bounds_box_width": range(10, 51, 10),  # 10, 20, 30, 40, 50
+            "bounds_box_width": range(11, 52, 10),  # 11 → 51, step 10
+            "bounds_mad_sigma": range(4, 8),  # 4 → 7
+            "bounds_mad_box_width": range(11, 52, 10),  # 11 → 51, step 10
         }
 
         for key, default in defaults.items():
@@ -72,23 +103,24 @@ class S3optMetaClass(MetaClass):
 
 
     def set_photometric_defaults(self):
-        '''Set Optimizer specific defaults for generic photometric data.
+        '''
+        Set Optimizer specific defaults for generic photometric data.
         '''
         return
 
 
     def set_MIRI_defaults(self):
-        '''Set Optimizer specific defaults for MIRI.
+        '''
+        Set Optimizer specific defaults for MIRI.
         '''
         defaults = {
             "bounds_bg_hw": range(5, 16),
             "bounds_spec_hw": range(1, 10),
-            "bounds_bg_deg": range(-1, 2),  # -1 → 1
         }
 
         for key, default in defaults.items():
             setattr(self, key, getattr(self, key, default))
-        self.bounds_bg_hw__spec_hw = getattr(self, 'bounds_bg_hw__spec_hw',
+        self.bounds_spec_hw__bg_hw = getattr(self, 'bounds_spec_hw__bg_hw',
                                              [self.bounds_bg_hw,
                                               self.bounds_spec_hw])
 
@@ -96,46 +128,48 @@ class S3optMetaClass(MetaClass):
 
 
     def set_NIRCam_defaults(self):
-        '''Set Optimizer specific defaults for NIRCam.
+        '''
+        Set Optimizer specific defaults for NIRCam.
         '''
         defaults = {
             "bounds_bg_hw": range(5, 16),
             "bounds_spec_hw": range(1, 10),
-            "bounds_bg_deg": range(-1, 2),  # -1 → 1
         }
 
         for key, default in defaults.items():
             setattr(self, key, getattr(self, key, default))
-        self.bounds_bg_hw__spec_hw = getattr(self, 'bounds_bg_hw__spec_hw',
+        self.bounds_spec_hw__bg_hw = getattr(self, 'bounds_spec_hw__bg_hw',
                                              [self.bounds_bg_hw,
                                               self.bounds_spec_hw])
 
         self.set_spectral_defaults()
+
 
     def set_NIRSpec_defaults(self):
-        '''Set Optimizer specific defaults for NIRSpec.
+        '''
+        Set Optimizer specific defaults for NIRSpec.
         '''
         defaults = {
             "bounds_bg_hw": range(5, 16),
             "bounds_spec_hw": range(1, 10),
-            "bounds_bg_deg": range(-1, 2),  # -1 → 1
         }
 
         for key, default in defaults.items():
             setattr(self, key, getattr(self, key, default))
-        self.bounds_bg_hw__spec_hw = getattr(self, 'bounds_bg_hw__spec_hw',
+        self.bounds_spec_hw__bg_hw = getattr(self, 'bounds_spec_hw__bg_hw',
                                              [self.bounds_bg_hw,
                                               self.bounds_spec_hw])
 
         self.set_spectral_defaults()
 
+
     def set_NIRISS_defaults(self):
-        '''Set Optimizer specific defaults for NIRISS.
+        '''
+        Set Optimizer specific defaults for NIRISS.
         '''
         defaults = {
             "bounds_bg_hw": range(15, 20),
             "bounds_spec_hw": range(17, 25),
-            "bounds_bg_deg": range(0, 2),  # 0 → 1
         }
 
         for key, default in defaults.items():
@@ -143,18 +177,24 @@ class S3optMetaClass(MetaClass):
 
         self.set_spectral_defaults()
 
+
     def set_WFC3_defaults(self):
-        '''Set Optimizer specific defaults for WFC3.
+        '''
+        Set Optimizer specific defaults for WFC3.
         '''
         self.set_spectral_defaults()
 
+
     def set_NIRCam_Photometry_defaults(self):
-        '''Set Optimizer specific defaults for NIRCam Photometry.
+        '''
+        Set Optimizer specific defaults for NIRCam Photometry.
         '''
         self.set_photometric_defaults()
 
+
     def set_MIRI_Photometry_defaults(self):
-        '''Set Optimizer specific defaults for MIRI Photometry.
+        '''
+        Set Optimizer specific defaults for MIRI Photometry.
         '''
 
         self.set_photometric_defaults()
