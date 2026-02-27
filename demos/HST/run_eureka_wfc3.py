24de2d065c7680c74f0db3c65d7a1996c6764bba
Taylor J Bell <tbell@stsci.edu>
2026-02-27 14:55:58 -0500

    Setting CRDS_SERVER_URL in demo run_eureka_wfc3.py

--- demos/HST/run_eureka_wfc3.py
+++ demos/HST/run_eureka_wfc3.py
@@ -1,4 +1,5 @@
 import os
+os.environ['CRDS_SERVER_URL'] = 'https://hst-crds.stsci.edu'
 import eureka.lib.plots
 import eureka.S3_data_reduction.s3_reduce as s3
 import eureka.S4_generate_lightcurves.s4_genLC as s4
