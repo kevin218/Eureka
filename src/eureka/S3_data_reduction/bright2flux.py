041be9b2af4fced001302bcceb1e9e1742e758d2
Taylor Bell <bell@baeri.org>
2026-05-04 23:43:39 -0400

    Fixing meta.gain application inside dn2electrons function

--- src/eureka/S3_data_reduction/bright2flux.py
+++ src/eureka/S3_data_reduction/bright2flux.py
@@ -113,9 +113,9 @@ def dn2electrons(data, meta, log):
         # Get the gain subarray
         gain = gain[meta.ywindow[0]:meta.ywindow[1],
                     meta.xwindow[0]:meta.xwindow[1]]
+        gain = xr.DataArray(gain, dims=("y", "x"))
 
     # Convert to electrons
-    gain = xr.DataArray(gain, dims=("y", "x"))
     data['flux'] *= gain
     data['err'] *= gain
     data['v0'] *= (gain)**2  # FINDME: should this really be squared
