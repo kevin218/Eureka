80b04b9a1af316913bb67d7af6bea8aa0694d5e1
Kevin Stevenson <kbstevenson@gmail.com>
2026-05-12 13:00:10 -0400

    Update defaults for S1opt

--- src/eureka/optimizer/S1opt_meta.py
+++ src/eureka/optimizer/S1opt_meta.py
@@ -87,19 +87,14 @@ class S1optMetaClass(MetaClass):
         '''
         Set Optimizer specific defaults for generic spectroscopic data.
         '''
-        full_list = ['jump_rejection_threshold', 'expand_mask', 'bg_deg',
-                     'bg_method', 'p3thresh', 'window_len']
+        full_list = ['jump_rejection_threshold', 'expand_mask', 'window_len']
         self.params_to_optimize_s1 = getattr(self, 'params_to_optimize_s1',
                                              full_list)
         # Spectral extraction parameters
         defaults = {
             # Stage 1
             "sweep_jump_rejection_threshold": range(4, 15),    # 4 → 14
-            "sweep_bg_method": ["std", "mean", "median"],
-            "sweep_bg_deg": range(0, 2),  # 0 → 1
-            "sweep_p3thresh": range(3, 8),  # 3 → 7
             "sweep_window_len": range(1, 22, 2),  # 1 → 21, odd only
-            "sweep_p7thresh": range(5, 61, 5),  # 5 → 60, step 5
             "sweep_expand_mask": range(5, 12),  # 5 → 11
         }
 
@@ -114,8 +109,15 @@ class S1optMetaClass(MetaClass):
         self.scaling_MAD_spec = getattr(self, 'scaling_MAD_spec', 0.01)
         self.scaling_MAD_white = getattr(self, 'scaling_MAD_white', 1.0)
 
+        full_list = ['jump_rejection_threshold', 'expand_mask', 'bg_deg',
+                     'bg_method', 'p3thresh', 'window_len']
+        self.params_to_optimize_s1 = getattr(self, 'params_to_optimize_s1',
+                                             full_list)
+
         defaults = {
-            "sweep_bg_hw": range(5, 16),
+            "sweep_bg_method": ["std", "mean", "median"],
+            "sweep_bg_deg": range(0, 2),  # 0 → 1
+            "sweep_p3thresh": range(3, 8),  # 3 → 7
         }
 
         for key, default in defaults.items():
@@ -145,7 +147,25 @@ class S1optMetaClass(MetaClass):
         '''
         Set Optimizer specific defaults for NIRCam.
         '''
-        pass
+        # Fitness scaling factors
+        self.scaling_MAD_spec = getattr(self, 'scaling_MAD_spec', 0.01)
+        self.scaling_MAD_white = getattr(self, 'scaling_MAD_white', 1.0)
+
+        full_list = ['jump_rejection_threshold', 'expand_mask', 'bg_deg',
+                     'bg_method', 'p3thresh', 'window_len']
+        self.params_to_optimize_s1 = getattr(self, 'params_to_optimize_s1',
+                                             full_list)
+
+        defaults = {
+            "sweep_bg_method": ["std", "mean", "median"],
+            "sweep_bg_deg": range(0, 2),  # 0 → 1
+            "sweep_p3thresh": range(3, 8),  # 3 → 7
+        }
+
+        for key, default in defaults.items():
+            setattr(self, key, getattr(self, key, default))
+
+        self.set_spectral_defaults()
 
     def set_NIRISS_defaults(self):
         '''
